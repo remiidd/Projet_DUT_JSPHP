097d21ec0f1b85ab3a0900e76f3bf24e2eed5363
Rémi Debray <remi281099@orange.fr>
2018-10-24 12:06:20 +0200

    Update

--- www/index.php
+++ www/index.php
@@ -3,9 +3,12 @@
   <head>
     <meta charset="utf-8">
     <title>Projet DUT</title>
+    <link rel="stylesheet" href="css/styles.css"/>
   </head>
   <body>
     <h1>index serveur Wamp</h1>
-    <p>test</p>
+    <p><?php echo "test php";?></p>
+    <script src="scripts/js/script.js"></script>
+    <?php include 'scripts/php/script.php'; ?>
   </body>
 </html>
