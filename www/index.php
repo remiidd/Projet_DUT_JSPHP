36935f8f3b961b47ae1d7b396df986485e474b1a
Rémi Debray <remi281099@orange.fr>
2018-10-24 17:31:16 +0200

    24-10-18 17:31:16

--- www/index.php
+++ www/index.php
@@ -7,7 +7,7 @@
   </head>
   <body>
     <h1>index serveur Wamp</h1>
-    <p>test bite<p>
+    <p>test test<p>
     <?php echo "test php";?></p>
     <script src="scripts/js/script.js"></script>
     <?php include 'scripts/php/script.php'; ?>
