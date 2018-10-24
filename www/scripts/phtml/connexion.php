7d94e9150517035a444100f1d2055e6c6a07419f
aloisguitton <aloisguitton@orange.fr>
2018-10-24 17:53:07 +0200

    24/10/2018 17:53:07

---
+++ www/scripts/phtml/connexion.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="fr">
+  <head>
+    <meta charset="utf-8">
+    <title>Connexion</title>
+  </head>
+  <body>
+    <div id="connexion">
+      <center>
+        
+      </center>
+    </div>
+  </body>
+</html>
