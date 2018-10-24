ca3a3f084fdbc567d1d1ff1b1fab442ec42a87b4
Rémi Debray <remi281099@orange.fr>
2018-10-24 17:24:09 +0200

    update

--- www/index.php
+++ www/index.php
@@ -7,7 +7,7 @@
   </head>
   <body>
     <h1>index serveur Wamp</h1>
-    <p>gros sexe<p>
+    <p>gros sexe poilu<p>
     <?php echo "test php";?></p>
     <script src="scripts/js/script.js"></script>
     <?php include 'scripts/php/script.php'; ?>
