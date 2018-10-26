e15bae2fd69050f57e731ecdbdef2821e7e6373b
Rémi Debray <remi281099@orange.fr>
2018-10-26 13:34:48 +0200

    26-10-18 13:34:48

--- www/profil.php
+++ www/profil.php
@@ -8,7 +8,7 @@
     <?php
 
       $bdd = new PDO('mysql:host=91.216.107.164;dbname=debra756602;charset=utf8','debra756602','hj32095D');
-
+      
       //$reponse = $bdd->query('SELECT * FROM profil WHERE id=\''.$_GET['id'].'\'');
       //$data = $reponse->fetch();
     ?>
