7463ac57c43c4cfb636553d396a46c5ef304d2fe
Rémi Debray <remi281099@orange.fr>
2018-10-26 17:05:25 +0200

    26-10-18 17:05:25

--- www/profil.php
+++ www/profil.php
@@ -36,6 +36,7 @@
     <div class="content">
       <div id="presentation">
         <img class="profilpicture" src="src/media/default_profil_picture.jpg" alt="Default profil picture"/>
+        <img class="profilcover" src="src/media/default_profil_cover.jpg" alt="Default profil cover"/>
       </div>
       <div id="infos"></div>
 
