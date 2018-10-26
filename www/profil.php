ba70f1e7da0f712bdb45aeb12e6bff61cd602a51
Rémi Debray <remi281099@orange.fr>
2018-10-26 13:18:51 +0200

    26-10-18 13:18:50

--- www/profil.php
+++ www/profil.php
@@ -8,8 +8,9 @@
     <?php
       $bdd = new PDO('mysql:host=91.216.107.164;dbname=debra756602_19tfmd;charset=utf8', 'debra756602_19tfmd', 'root');
 
-      $reponse = $bdd->query('');
+      $reponse = $bdd->query('SELECT * FROM profil WHERE id=\''.$_GET['id'].'\'');
+      $data = $reponse->fetch();
     ?>
-    <p>Utilisateur avec l'id : <?php echo $_GET["id"]; ?></p>
+    <p>Utilisateur avec l'id : <?php echo $_GET["id"]; ?> s'appelle <?php echo $data["prenom"]." ".$data["nom"]; ?></p>
   </body>
 </html>
