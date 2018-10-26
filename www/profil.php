77531235a8adac7f4291f660414dbd0d979dffb4
Rémi Debray <remi281099@orange.fr>
2018-10-26 13:09:41 +0200

    26-10-18 13:09:41

--- www/profil.php
+++ www/profil.php
@@ -6,5 +6,8 @@
   </head>
   <body>
     <p>Utilisateur avec l'id : <?php echo $_GET["id"]; ?></p>
+    <?php
+      $bdd = new PDO('mysql:host=91.216.107.164;dbname=debra756602_19tfmd;charset=utf8', 'debra756602_19tfmd', 'root');
+    ?>
   </body>
 </html>
