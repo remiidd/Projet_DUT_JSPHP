64ec02b60c5f9609d02ee977304f1925383847f7
Rémi Debray <remi281099@orange.fr>
2018-10-26 16:19:17 +0200

    26-10-18 16:19:16

--- www/profil.php
+++ www/profil.php
@@ -6,8 +6,13 @@
   </head>
   <body>
     <?php
-
+    try {
       $bdd = new PDO('mysql:host=lulipa.server.r-heberg.fr;dbname=debrayalois;charset=utf8', 'debrayalois', 'testdebrayalois');
+    } catch (\Exception $e) {
+      die(e->getMessage());
+    }
+
+
 
       //$reponse = $bdd->query('SELECT * FROM profil WHERE id=\''.$_GET['id'].'\'');
       //$data = $reponse->fetch();
