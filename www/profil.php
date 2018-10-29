08ec94cfceccbd53cd341c8b3d6db9b713e6b9c9
Rémi Debray <remi281099@orange.fr>
2018-10-29 21:16:56 +0100

    29-10-18 21:16:56

--- www/profil.php
+++ www/profil.php
@@ -45,7 +45,7 @@
         <?php if(($today->format("%m%d"))==($naissance->format("%m%d"))) { ?><p>Bon anniversaire !</p><?php } ?>
       </div>
       <div class="envoyer_post">
-        <form class="form_envoyer_post" action="#" method="post">
+        <form class="form_envoyer_post" action="" method="post">
           <textarea name="textarea_posts"></textarea>
           <input type="submit" name="bouton_posts" value="Bananez !"/>
         </form>
