d731b3c9ab70902a2903b0364d5b9d2bf3f64740
Rémi Debray <remi281099@orange.fr>
2018-10-29 22:22:33 +0100

    29-10-18 22:22:33

--- www/profil.php
+++ www/profil.php
@@ -52,14 +52,14 @@
         <?php
           if(isset($_POST["textarea_posts"])) {
             $today = new DateTime();
-            $req = $bdd->prepare('INSERT INTO posts(id, nom_createur, date_publication, contenu, photo, profil, nb_com, nb_like, nb_share) VALUES('', :noms, :date_publi, :contenu, :photo, :profil,'0','0','0')');
-            /*$req->execute(array(
+            $req = $bdd->prepare('INSERT INTO posts(id, nom_createur, date_publication, contenu, photo, profil, nb_com, nb_like, nb_share) VALUES(\'\', :noms, :date_publi, :contenu, :photo, :profil,\'0\',\'0\',\'0\')');
+            $req->execute(array(
               'noms' => $_POST["prenom"]." ".$data["nom"],
               'date_publi' => $today->format("%Y-%m-%d"),
               'contenu' => $message,
               'photo' => "",
               'profil' => $_GET["id"]
-            ));*/
+            ));
             echo "yes!";
           }
         ?>
