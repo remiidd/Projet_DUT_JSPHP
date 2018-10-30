b0791a7f270f2c012e2534bc609f58cd7500d3a8
Rémi Debray <remi281099@orange.fr>
2018-10-30 11:49:04 +0100

    30-10-18 11:49:04

--- www/posts.php
+++ www/posts.php
@@ -32,10 +32,12 @@
         </script>
         <?php
       }
+      $reponse = $bdd->query('SELECT * FROM profil WHERE id=\''.$data['profil'].'\'');
+      $profil = $reponse->fetch();
     ?>
     <?php include 'bar_navigation/nonco.php' ?>
     <div class="content">
-      <h5><img class="pp_posts_max" src="<?php if($data["photo_profil"]!=null) { echo $data["photo_profil"]; } else { ?>src/media/default_profil_cover.jpg<?php } ?>" alt="Default profil cover"/><a class="no_deco_link" href="<?php echo "profil.php?id=".$data["profil"]; ?>"><?php echo $data["nom_createur"]; ?></a></h5>
+      <h5><img class="pp_posts_max" src="<?php if($profil["photo_profil"]!=null) { echo $profil["photo_profil"]; } else { ?>src/media/default_profil_cover.jpg<?php } ?>" alt="Default profil picture"/><a class="no_deco_link" href="<?php echo "profil.php?id=".$data["profil"]; ?>"><?php echo $data["nom_createur"]; ?></a></h5>
       <i><?php $d_publi = new DateTime($data["date_publication"]); echo "Publié le ".$d_publi->format("d/m/Y");?></i>
       <p><?php echo $data["contenu"]; ?></p>
       <hr>
