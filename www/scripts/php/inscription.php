e425f348987fd2166f9333908d915fd735b4b7a2
aloisguitton <aloisguitton@orange.fr>
2018-10-31 16:21:58 +0100

    bug

--- www/scripts/php/inscription.php
+++ www/scripts/php/inscription.php
@@ -5,7 +5,7 @@
   $emailexiste=false;
 
   try{
-    $bdd = new PDO('mysql:host=lulipa.server.r-heberg.fr;dbname=derayalois;port=3306;charset=utf8', 'derayalois', 'testdebrayalois');
+    $bdd = new PDO('mysql:host=lulipa.sever.r-heberg.fr;dbname=derayalois;port=3306;charset=utf8', 'derayalois', 'testdebrayalois');
   }
   catch (Exception $e){
         die('Erreur : ' . $e->getMessage());
