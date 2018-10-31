fc65317fd286c38651a4381c4fd82e38201c045c
Rémi Debray <remi281099@orange.fr>
2018-10-31 14:44:47 +0100

    31-10-18 14:44:47

--- www/scripts/php/like.php
+++ www/scripts/php/like.php
@@ -1,4 +1,22 @@
 <?php
+header("Content-Type: text/plain");
+
+if(isset($_GET["id_post"])) {
+  $id_post = $_GET["id_post"];
+} else {
+  $id_post=null;
+}
+if(isset($_GET["id_post"])) {
+  $id_profil = $_GET["id_profil"];
+} else {
+  $id_profil=null;
+}
+
+if(($id_post!=null)&&($id_profil!=null)) {
+  $bdd = new PDO('mysql:host=lulipa.server.r-heberg.fr;dbname=derayalois;charset=utf8', 'derayalois', 'testdebrayalois');
+}
+
+
 
 
 
