b20e0eaaa387979333a766b37349686602d01cdb
Rémi Debray <remi281099@orange.fr>
2018-10-31 15:21:15 +0100

    31-10-18 15:21:15

--- www/scripts/php/like.php
+++ www/scripts/php/like.php
@@ -14,6 +14,12 @@ if(isset($_GET["id_post"])) {
 
 if(($id_post!=null)&&($id_profil!=null)) {
   $bdd = new PDO('mysql:host=lulipa.server.r-heberg.fr;dbname=derayalois;charset=utf8', 'derayalois', 'testdebrayalois');
+  $reponse = $bdd->query('SELECT * FROM posts WHERE profil=\''.$_GET['id'].'\' ORDER BY id DESC');
+  while($feed = $reponse->fetch()){}
+
+
+
+
   $req = $bdd->prepare('INSERT INTO like_table(id, id_post, profil_like) VALUES(NULL, :id_post, :profil_like)');
   $req->execute(array(
     'id_post' => $id_post,
