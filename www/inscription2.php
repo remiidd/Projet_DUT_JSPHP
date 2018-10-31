ca7c325e394c4a31cdcf1e7163c11fed3700f8aa
aloisguitton <aloisguitton@orange.fr>
2018-10-31 16:47:15 +0100

    form inscri 2

--- www/inscription2.php
+++ www/inscription2.php
@@ -9,7 +9,10 @@
     <div class="contenu">
       <center>
         <form id="inscription2" action="" method="post">
-          <input type="text" name="nom" value="<?php echo $_SESSION['prenom']; ?>" readonly>
+          <input type="text" name="prenom" value="<?php echo $_SESSION['prenom']; ?>" readonly>
+          <input type="text" name="nom" value="<?php echo $_SESSION['nom']; ?>" readonly>
+          <input type="text" name="email" value="<?php echo $_SESSION['email']; ?>" readonly>
+          <input type="date" name="nom" value="<?php echo $_SESSION['naissance']; ?>" readonly>
         </form>
       </center>
     </div>
