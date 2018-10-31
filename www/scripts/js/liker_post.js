827dbcd4f21b4193ebfc199a15bb0e5428088c00
Rémi Debray <remi281099@orange.fr>
2018-10-31 15:01:59 +0100

    31-10-18 15:01:59

--- www/scripts/js/liker_post.js
+++ www/scripts/js/liker_post.js
@@ -24,6 +24,7 @@ function liker_post(id_posts, id_profils) {
 
   var id_post = encodeURIComponent(id_posts);
   var id_profil = encodeURIComponent(id_profils);
+  console.log("salut");
   try {
     xhr.open("GET", "scripts/php/like.php?id_post="+id_post+"&id_profil="+id_profil,true);
     xhr.send(null);
