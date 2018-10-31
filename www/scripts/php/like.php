b27827cdec8d008177baedbdbe173f50d135d4eb
Rémi Debray <remi281099@orange.fr>
2018-10-31 15:28:21 +0100

    31-10-18 15:28:21

--- www/scripts/php/like.php
+++ www/scripts/php/like.php
@@ -19,14 +19,16 @@ if(($id_post!=null)&&($id_profil!=null)) {
   $reponse = $bdd->query('SELECT * FROM like_table WHERE profil_like=\''.$id_profil.'\' AND id_post=\''.$id_post.'\'');
   while($rep = $reponse->fetch()){$ok=true;}
 
+  if($ok==false){
+    $req = $bdd->prepare('INSERT INTO like_table(id, id_post, profil_like) VALUES(NULL, :id_post, :profil_like)');
+    $req->execute(array(
+      'id_post' => $id_post,
+      'profil_like' => $id_profil
+    ));
+  }
+  if($ok){
 
-
-
-  $req = $bdd->prepare('INSERT INTO like_table(id, id_post, profil_like) VALUES(NULL, :id_post, :profil_like)');
-  $req->execute(array(
-    'id_post' => $id_post,
-    'profil_like' => $id_profil
-  ));
+  }
 }
 
 
