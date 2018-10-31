8a1332d8e38fff71c537a66f41bf26817e7e06e3
Rémi Debray <remi281099@orange.fr>
2018-10-31 14:28:32 +0100

    31-10-18 14:28:32

---
+++ www/scripts/js/liker_post.js
@@ -0,0 +1,28 @@
+function getXMLHttpRequest() {
+	var xhr = null;
+
+	if (window.XMLHttpRequest || window.ActiveXObject) {
+		if (window.ActiveXObject) {
+			try {
+				xhr = new ActiveXObject("Msxml2.XMLHTTP");
+			} catch(e) {
+				xhr = new ActiveXObject("Microsoft.XMLHTTP");
+			}
+		} else {
+			xhr = new XMLHttpRequest();
+		}
+	} else {
+		alert("Votre navigateur ne supporte pas l'objet XMLHTTPRequest...");
+		return null;
+	}
+	
+	return xhr;
+}
+
+function liker_post(id_posts, id_profils) {
+  var xhr = getXMLHttpRequest();
+
+  var id_post = encodeURIComponent(id_posts);
+  var id_profil = encodeURIComponent(id_profils);
+  xhr.open("GET")
+}
