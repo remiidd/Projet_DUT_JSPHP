3767dd0d696875d301b0917d88e89ad10a7a3d22
Rémi Debray <remi281099@orange.fr>
2018-11-01 23:41:04 +0100

    01-11-18 23:41:04

--- www/scripts/js/caractere_max.js
+++ www/scripts/js/caractere_max.js
@@ -1,1 +1,8 @@
-var text_post = document.getElementById('');
+var text_post = document.getElementById('areapost');
+
+text_post.addEventListener("input", function(e){
+  document.getElementById('nb_caract').innerHTML = text_post.value.length;
+  if(text_post.value.length > 500){
+    document.getElementById('nb_caract_string').style.color = 'red';
+  }
+});
