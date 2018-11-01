592089965b59122990aef4ceadfc8e519b7cb837
aloisguitton <aloisguitton@orange.fr>
2018-11-01 20:50:26 +0100

    01/11/2018 20:50:26

--- www/inscription2.php
+++ www/inscription2.php
@@ -11,37 +11,37 @@
         <form id="inscription2" action="" method="post">
           <input type="text" name="prenom" placeholder="<?php echo $_SESSION['prenom']; ?>" readonly>
           <br>
-          <input type="text" name="nom" value="<?php echo $_SESSION['nom']; ?>" readonly>
+          <input type="text" name="nom" placeholder="<?php echo $_SESSION['nom']; ?>" readonly>
           <br>
-          <input type="text" name="email" value="<?php echo $_SESSION['email']; ?>" readonly>
+          <input type="text" name="email" placeholder="<?php echo $_SESSION['email']; ?>" readonly>
           <br>
-          <input type="date" name="nom" value="<?php echo $_SESSION['naissance']; ?>" readonly>
+          <input type="date" name="nom" placeholder="<?php echo $_SESSION['naissance']; ?>" readonly>
           <br>
           <?php
             if($_SESSION['sexe'] == "Homme"){
               ?>
               <div id="formSexe2">
-                <input readonly required type="radio" name="sexe" value="homme" checked>Homme
-                <input readonly required type="radio" name="sexe" value="femme" disabled>Femme
-                <input readonly required type="radio" name="sexe" value="banana" disabled>BANANAAAAAAAA<br>
+                <input readonly required type="radio" name="sexe" placeholder="homme" checked>Homme
+                <input readonly required type="radio" name="sexe" placeholder="femme" disabled>Femme
+                <input readonly required type="radio" name="sexe" placeholder="banana" disabled>BANANAAAAAAAA<br>
               </div>
               <?php
             }
             elseif ($_SESSION['sexe'] == "Femme") {
               ?>
               <div id="formSexe2">
-                <input readonly required type="radio" name="sexe" value="homme" disabled>Homme
-                <input readonly required type="radio" name="sexe" value="femme" checked>Femme
-                <input readonly required type="radio" name="sexe" value="banana" disabled>BANANAAAAAAAA<br>
+                <input readonly required type="radio" name="sexe" placeholder="homme" disabled>Homme
+                <input readonly required type="radio" name="sexe" placeholder="femme" checked>Femme
+                <input readonly required type="radio" name="sexe" placeholder="banana" disabled>BANANAAAAAAAA<br>
               </div>
               <?php
             }
             else{
               ?>
               <div id="formSexe2">
-                <input readonly required type="radio" name="sexe" value="homme" disabled>Homme
-                <input readonly required type="radio" name="sexe" value="femme" disabled>Femme
-                <input readonly required type="radio" name="sexe" value="banana" checked>BANANAAAAAAAA<br>
+                <input readonly required type="radio" name="sexe" placeholder="homme" disabled>Homme
+                <input readonly required type="radio" name="sexe" placeholder="femme" disabled>Femme
+                <input readonly required type="radio" name="sexe" placeholder="banana" checked>BANANAAAAAAAA<br>
               </div>
               <?php
             }
