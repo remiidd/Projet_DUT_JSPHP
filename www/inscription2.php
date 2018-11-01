0c097531d415d4dca8575d67b332022be2e36a92
aloisguitton <aloisguitton@orange.fr>
2018-11-01 21:43:33 +0100

    01/11/2018 21:43:33

--- www/inscription2.php
+++ www/inscription2.php
@@ -47,7 +47,7 @@
             }
           ?>
           <input type="tel" name="tel" placeholder="Téléphone">
-          <input type="image" name="" value="">
+          <input type="image" name="" value="photo">
         </form>
       </center>
     </div>
