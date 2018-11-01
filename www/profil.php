667ae63018284f2cc79022581dca85752f8d5ae5
Rémi Debray <remi281099@orange.fr>
2018-11-01 22:43:43 +0100

    01-11-18 22:43:43

--- www/profil.php
+++ www/profil.php
@@ -11,9 +11,6 @@
     <!--Fin des fichiers bootstrap-->
     <script src="scripts/js/liker_post.js"></script>
     <link rel="stylesheet" href="css/styles.css"/>
-    <script src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/3/autoresize.jquery.js">
-      $('textarea').autoResize();
-    </script>
   </head>
   <body>
     <?php
