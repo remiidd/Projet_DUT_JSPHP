5b4c68df72e6cdfc7cc5989627b98b4fb371a65a
Rémi Debray <remi281099@orange.fr>
2018-11-01 23:57:47 +0100

    01-11-18 23:57:47

--- www/scripts/js/caractere_max.js
+++ www/scripts/js/caractere_max.js
@@ -4,10 +4,10 @@ text_post.addEventListener("input", function(e){
   document.getElementById('nb_caract').innerHTML = text_post.value.length;
   if(text_post.value.length > 500){
     document.getElementById('nb_caract_string').style.color = 'red';
-    document.getElementById('nb_caract_string').disabled = true;
+    document.getElementById('inscriBout').disabled = true;
   }
   if(text_post.value.length <= 500){
     document.getElementById('nb_caract_string').style.color = 'black';
-    document.getElementById('nb_caract_string').disabled = false;
+    document.getElementById('inscriBout').disabled = false;
   }
 });
