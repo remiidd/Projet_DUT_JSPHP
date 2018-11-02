64f1112fe05477043bbfc35a0b9070f1084695f4
aloisguitton <aloisguitton@orange.fr>
2018-11-02 14:02:45 +0100

    aze

--- www/scripts/php/connexion.php
+++ www/scripts/php/connexion.php
@@ -22,7 +22,7 @@
       echo "succes" . $donnees['id'];
       $_SESSION['idcon'] = $donnees['id'];
       echo "-session : " . $_SESSION['idcon'];
-      header("Location:../../profil.php?id=$_SESSION['idcon']");
+      header('Location:../../index.php');
       exit();
     }
     else {
