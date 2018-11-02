0706071dc3743f6330a51b499b7ee7d00d2bcf6b
aloisguitton <aloisguitton@orange.fr>
2018-11-02 14:14:36 +0100

    idcon

--- www/index.php
+++ www/index.php
@@ -1,6 +1,6 @@
 <?php session_start();
-  if(isset($_SESSION['id'])){
-    $id = $_SESSION['id'];
+  if(isset($_SESSION['idcon'])){
+    $id = $_SESSION['idcon'];
     header("Location:../../profil.php?id=$id");
     exit();
   }
