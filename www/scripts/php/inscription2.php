ed26732f95c1691ecd32fe41ea8f21c4d68ff850
aloisguitton <aloisguitton@orange.fr>
2018-11-02 09:51:01 +0100

    erst

--- www/scripts/php/inscription2.php
+++ www/scripts/php/inscription2.php
@@ -52,6 +52,7 @@
   $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
   // Check if image file is a actual image or fake image
   $resultat = move_uploaded_file($_FILES['pp']['tmp_name'], $target_file);
+  echo $resultat;
   if ($resultat) echo "Transfert réussi";
 
 ?>
