15bd052a4ec67ac5b48f316cdc9774f8b4b5d22a
Rémi Debray <remi281099@orange.fr>
2018-11-02 12:24:01 +0100

    02-11-18 12:24:01

--- www/settings.php
+++ www/settings.php
@@ -1,4 +1,9 @@
 <!DOCTYPE html>
+<?php session_start();
+if((!isset($_SESSION["idcon"]))||($_SESSION["idcon"]!=$_GET["id"])){
+  header("Location: index.php");
+}
+?>
 <html lang="fr">
   <head>
     <meta charset="utf-8">
