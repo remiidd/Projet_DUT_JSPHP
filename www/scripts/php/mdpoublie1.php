aeb2c3f28e3693ae2d9d88f0ef6d85dec5c7cab2
aloisguitton <aloisguitton@orange.fr>
2018-11-04 22:04:26 +0100

    04/11/2018 22:04:25

--- www/scripts/php/mdpoublie1.php
+++ www/scripts/php/mdpoublie1.php
@@ -35,11 +35,45 @@
     }
 
     if ($mailok != "") {
-      echo "Trouve";
+
+      $mail = new PHPMailer(TRUE);
+
+      try{
+        $mail->CharSet = 'UTF-8';
+        $mail->SMTPDebug = 2;
+        $mail->isSMTP();
+        $mail->Host = 'smtp.gmail.com';
+        $mail->SMTPAuth = true;
+        $mail->Username = 'bananabook.contact@gmail.com';
+        $mail->Password = 'mailbanana';
+        $mail->SMTPSecure = 'tls';
+        $mail->Port = 587;
+
+        $mail->setFrom('bananabook.contact@gmail.com', 'BananaBook');
+        $mail->addAddress($mailok);
+
+        $mail->isHTML(true);
+        $mail->Subject = 'Reinitialisation de votre mot de passe';
+        $mail->Body    = "Bonjour $prenom, </br>
+          Est-ce que tu aimes les bananes ? </br>
+          Pour reinitialiser votre mot de passe, veuillez cliquer sur le lien ci-dessous : </br>
+          <a href=\"http://nunes.aloisguitton.com/scripts/php/mdpoublie.php?code=$code\">nunes.aloisguitton.comscripts/php/mdpoublie.php?code=$code</a></br></br></br>
+          <cite>Cet email a ete envoye automatiquement depuis <a href=\"http://nunes.aloisguitton.com\">BananaBook</a>. Ne pas repondre </cite>";
+        $mail->AltBody = 'Bonjour Rémi, Est-ce que tu aimes les bananes ?
+          Cet email a ete envoye automatiquement depuis BananaBook. Ne pas repondre';
+
+        $mail->send();
+        echo 'Message has been sent';
+      } catch (Exception $e) {
+          echo 'Message could not be sent.';
+          echo 'Mailer Error: ' . $mail->ErrorInfo;
+      }
+      
     }
     else {
       echo "aucun utilisateur trouvé";
     }
+
 
 
 
@@ -50,37 +84,6 @@
 
 
 
-  $mail = new PHPMailer(TRUE);
 
-  try{
-    $mail->CharSet = 'UTF-8';
-    $mail->SMTPDebug = 2;
-    $mail->isSMTP();
-    $mail->Host = 'smtp.gmail.com';
-    $mail->SMTPAuth = true;
-    $mail->Username = 'bananabook.contact@gmail.com';
-    $mail->Password = 'mailbanana';
-    $mail->SMTPSecure = 'tls';
-    $mail->Port = 587;
-
-    $mail->setFrom('bananabook.contact@gmail.com', 'BananaBook');
-    $mail->addAddress($mailok);
-
-    $mail->isHTML(true);
-    $mail->Subject = 'Reinitialisation de votre mot de passe';
-    $mail->Body    = "Bonjour $prenom, </br>
-      Est-ce que tu aimes les bananes ? </br>
-      Pour reinitialiser votre mot de passe, veuillez cliquer sur le lien ci-dessous : </br>
-      <a href=\"http://nunes.aloisguitton.com/scripts/php/mdpoublie.php?code=$code\">nunes.aloisguitton.comscripts/php/mdpoublie.php?code=$code</a></br></br></br>
-      <cite>Cet email a ete envoye automatiquement depuis <a href=\"http://nunes.aloisguitton.com\">BananaBook</a>. Ne pas repondre </cite>";
-    $mail->AltBody = 'Bonjour Rémi, Est-ce que tu aimes les bananes ?
-      Cet email a ete envoye automatiquement depuis BananaBook. Ne pas repondre';
-
-    $mail->send();
-    echo 'Message has been sent';
-  } catch (Exception $e) {
-      echo 'Message could not be sent.';
-      echo 'Mailer Error: ' . $mail->ErrorInfo;
-  }
 
 ?>
