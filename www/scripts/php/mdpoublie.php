1eaf30ba159f3b4399e76927a7b04df0b87d4794
aloisguitton <aloisguitton@orange.fr>
2018-11-04 21:04:15 +0100

    04/11/2018 21:04:14

--- www/scripts/php/mdpoublie.php
+++ www/scripts/php/mdpoublie.php
@@ -42,14 +42,14 @@
   $email = $_POST['email'];
 
   echo $email;
-  /*
+
   try{
     $bdd = new PDO('mysql:host=lulipa.server.r-heberg.fr;dbname=derayalois;port=3306;charset=utf8', 'derayalois', 'testdebrayalois');
   }
   catch (Exception $e){
         die('Erreur : ' . $e->getMessage());
   }
-
+  /*
   //variables
   $reponse = $bdd->query('SELECT * FROM profil');
 
