19d52c93b2660e9f54176679e9969629ce01dcb0
aloisguitton <aloisguitton@orange.fr>
2018-11-09 11:49:36 +0100

    daz

--- www/scripts/php/mdpoublie.php
+++ www/scripts/php/mdpoublie.php
@@ -21,37 +21,7 @@
 
     if ($id != "") {
       // chaine connu
-      ?>
-        <!DOCTYPE html>
-        <html lang="fr">
-          <head>
-            <meta charset="utf-8">
-            <title>Réinitialiser votre mot de passe</title>
-            <?php  include 'head.html';?>
-          </head>
-          <body>
-            <?php include '../../bar_navigation/connexion.php'; ?>
-            <div class="content">
-              <div id="mdpoublié">
-                <center>
-                  <form class="form_mdpo" action="scripts/php/resetmdp.php" method="post">
-                    <p>
-                      <a id="passerrorr">Le s mots de passe ne correspondent pas</a>
-                      <br>
-                      <input id="mdpr" required class="inscriform" type="password" name="mdp" placeholder="Mot de passe">
-                      </br>
-                      <input id="mdpr1" required class="inscriform" type="password" name="mdprepeat" placeholder="Retapper votre mot de passe">
-                      </br>
-                    </p>
-                    <input type="submit" value="Changer mon mot de passe">
-                  </form>
-                </center>
-              </div>
-            </div>
-          </body>
-          <script src="scripts/js/reset.js"></script>
-        </html>
-      <?php
+      include "../html/resetmdp.php";
     }
     else {
       //chaine non-reconnue
