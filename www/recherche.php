41a872d689da1c6dbfe1ad310faa7d74f569cc37
aloisguitton <aloisguitton@orange.fr>
2018-11-09 23:34:34 +0100

    09/11/2018 23:34:34

---
+++ www/recherche.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Recherche</title>
+    <?php include 'scripts/html/head.html'; ?>
+  </head>
+  <body>
+    <?php include 'bar_navigation/nonco'?>
+  </body>
+</html>
