1650fb10b42b868023936c8a60e66dc95cfb917e
aloisguitton <aloisguitton@orange.fr>
2018-11-09 11:34:39 +0100

    azd

--- www/scripts/php/mdpoublie.php
+++ www/scripts/php/mdpoublie.php
@@ -1,7 +1,9 @@
 <?php
   if(isset($_GET['code'])){
-/*
+
     $id = ""
+
+    echo "ici";/*
 
     try{
       $bdd = new PDO('mysql:host=lulipa.server.r-heberg.fr;dbname=derayalois;port=3306;charset=utf8', 'derayalois', 'testdebrayalois');
