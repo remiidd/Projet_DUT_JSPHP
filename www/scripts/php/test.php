7b7d26b0e1c7b6a510701d62a9c78efbfbd525b5
aloisguitton <aloisguitton@orange.fr>
2018-11-09 10:58:46 +0100

    zad

--- www/scripts/php/test.php
+++ www/scripts/php/test.php
@@ -1,13 +1,9 @@
 <?php
-function generateRandomString() {
   $caract = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
   $caract_long = strlen($caract);
   $randomString = '';
   for ($i = 0; $i < 10; $i++) {
-      $randomString .= $caract[rand(0, $characte$caract_longrsLength - 1)];
+      $randomString .= $caract[rand(0, $caract_long - 1)];
   }
   echo $randomString;
-}
-
-generateRandomString();
 ?>
