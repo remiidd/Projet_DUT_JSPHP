da64fc0e61fddd62f289f97fd894de4157870532
aloisguitton <aloisguitton@orange.fr>
2018-11-09 23:16:17 +0100

    09/11/2018 23:16:17

--- www/scripts/php/resetmdp.php
+++ www/scripts/php/resetmdp.php
@@ -1,6 +1,7 @@
 <?php
   session_start();
   $password = md5($_POST['mdp']);
+  $id =  $_SESSION['idmdpo'];
 
   try{
     $bdd = new PDO('mysql:host=lulipa.server.r-heberg.fr;dbname=derayalois;port=3306;charset=utf8', 'derayalois', 'testdebrayalois');
@@ -9,7 +10,7 @@
         die('Erreur : ' . $e->getMessage());
   }
 
-  $bdd->exec("UPDATE `profil` SET `password` = '$password' WHERE `id` = 19");
+  $bdd->exec("UPDATE `profil` SET `password` = '$password' WHERE `id` = $id");
 
   echo "ok";
   //$bdd->exec("DELETE FROM `mdpoublie` WHERE `chaine_id` = $_SESSION['idmdpo']");
