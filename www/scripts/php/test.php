a010ec6d47bdce65ee67f2a7ca7433d1da6bfa95
aloisguitton <aloisguitton@orange.fr>
2018-11-09 11:01:57 +0100

    mdp et chaine + longue

--- www/scripts/php/test.php
+++ www/scripts/php/test.php
@@ -1,9 +1,13 @@
 <?php
+function generateRandomString() {
   $caract = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
   $caract_long = strlen($caract);
   $randomString = '';
-  for ($i = 0; $i < 10; $i++) {
+  for ($i = 0; $i < 20; $i++) {
       $randomString .= $caract[rand(0, $caract_long - 1)];
   }
-  echo $randomString;
+  return md5($randomString);
+}
+
+echo "str" . generateRandomString();
 ?>
