6f8eb15b1434c87d4708eb90c45e8a6da81c5936
aloisguitton <aloisguitton@orange.fr>
2018-11-09 11:47:57 +0100

    azd

--- www/scripts/php/mdpoublie.php
+++ www/scripts/php/mdpoublie.php
@@ -3,8 +3,6 @@
 
     $id = "";
 
-    echo "ici";
-
     try{
       $bdd = new PDO('mysql:host=lulipa.server.r-heberg.fr;dbname=derayalois;port=3306;charset=utf8', 'derayalois', 'testdebrayalois');
     }
@@ -23,7 +21,37 @@
 
     if ($id != "") {
       // chaine connu
-      include("../html/resetmdp.php");
+      ?>
+        <!DOCTYPE html>
+        <html lang="fr">
+          <head>
+            <meta charset="utf-8">
+            <title>Réinitialiser votre mot de passe</title>
+            <?php  include 'head.html';?>
+          </head>
+          <body>
+            <?php include '../../bar_navigation/connexion.php'; ?>
+            <div class="content">
+              <div id="mdpoublié">
+                <center>
+                  <form class="form_mdpo" action="scripts/php/resetmdp.php" method="post">
+                    <p>
+                      <a id="passerrorr">Le s mots de passe ne correspondent pas</a>
+                      <br>
+                      <input id="mdpr" required class="inscriform" type="password" name="mdp" placeholder="Mot de passe">
+                      </br>
+                      <input id="mdpr1" required class="inscriform" type="password" name="mdprepeat" placeholder="Retapper votre mot de passe">
+                      </br>
+                    </p>
+                    <input type="submit" value="Changer mon mot de passe">
+                  </form>
+                </center>
+              </div>
+            </div>
+          </body>
+          <script src="scripts/js/reset.js"></script>
+        </html>
+      <?php
     }
     else {
       //chaine non-reconnue
