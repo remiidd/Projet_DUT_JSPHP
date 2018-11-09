1e418e95d0110669a0f570895c97bd641135bb98
aloisguitton <aloisguitton@orange.fr>
2018-11-09 23:16:42 +0100

    09/11/2018 23:16:42

--- www/scripts/php/resetmdp.php
+++ www/scripts/php/resetmdp.php
@@ -13,7 +13,8 @@
   $bdd->exec("UPDATE `profil` SET `password` = '$password' WHERE `id` = $id");
 
   echo "ok";
-  //$bdd->exec("DELETE FROM `mdpoublie` WHERE `chaine_id` = $_SESSION['idmdpo']");
+
+  $bdd->exec("DELETE FROM `mdpoublie` WHERE `chaine_id` = $id");
 
 
 ?>
