3c3dab48748d6993b6c034ecd48719bfbbcb28ae
aloisguitton <aloisguitton@orange.fr>
2018-11-09 10:55:13 +0100

    test

---
+++ www/scripts/php/test.php
@@ -0,0 +1,13 @@
+<?php
+function generateRandomString() {
+  $caract = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  $caract_long = strlen($caract);
+  $randomString = '';
+  for ($i = 0; $i < 10; $i++) {
+      $randomString .= $characters[rand(0, $charactersLength - 1)];
+  }
+  return $randomString;
+}
+
+echo generateRandomString();
+?>
