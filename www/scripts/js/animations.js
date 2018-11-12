6d0d27f358b52e03bc5cf73ac909230ea13ab745
Rémi Debray <remi281099@orange.fr>
2018-11-12 15:36:56 +0100

    12-11-18 15:36:56

--- www/scripts/js/animations.js
+++ www/scripts/js/animations.js
@@ -1,11 +1,11 @@
 $(document).ready(function(){
-  $(".txt_modif_email").hide();
-  $(".txt_modif_num").hide();
-  
   $(".titre_param").animate({
     top:'120px',
     opacity:'1'
   },"slow");
+
+  $(".txt_modif_email").hide();
+  $(".txt_modif_num").hide();
 
 
   $(".modif_info_bouton").click(function(){
