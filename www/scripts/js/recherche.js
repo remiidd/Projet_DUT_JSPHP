da503eb318d1c62a186ac6a8b869586ced179b3a
aloisguitton <aloisguitton@orange.fr>
2018-11-21 17:04:26 +0100

    aze

--- www/scripts/js/recherche.js
+++ www/scripts/js/recherche.js
@@ -2,7 +2,7 @@ function resize_img(){
   var img = document.getElementsByClassName("rech_div_img");
   var div = document.getElementsByClassName("rech_user");
   var taille = document.body.clientWidth / 10;
-  var taille2 = 1.004 * (document.body.clientWidth / 10);
+  var taille2 = document.body.clientWidth / 10 + 0.004 * document.body.clientWidth;
   for (i = 0; i < img.length; i++) {
     img[i].style.width = taille + "px";
     img[i].style.height = taille + "px";
