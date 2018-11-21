63cd3f9f3620bf1c07ba57f51fd9be9d53f10939
aloisguitton <aloisguitton@orange.fr>
2018-11-21 16:43:54 +0100

    s

--- www/recherche.php
+++ www/recherche.php
@@ -29,7 +29,7 @@
         <head>
           <meta charset="utf-8">
           <title>Recherche</title>
-          <script src="script/js/recherche.js" charset="utf-8"></script>
+          <script src="scripts/js/recherche.js" charset="utf-8"></script>
           <?php include 'scripts/html/head.html'; ?>
         </head>
         <body onresize="resize_img()" onload="resize_img()">
