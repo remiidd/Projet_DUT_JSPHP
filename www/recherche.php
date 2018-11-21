50b952533d865c39cc0a5d3a503b30072ab62ec1
aloisguitton <aloisguitton@orange.fr>
2018-11-21 14:55:24 +0100

    erg

--- www/recherche.php
+++ www/recherche.php
@@ -1,16 +1,32 @@
 <?php
   session_start();
-  header('Content-type: text/html; charset=UTF-8');
+
+  function str_to_noaccent($str)
+  {
+      $url = $str;
+      $url = preg_replace('#Ç#', 'C', $url);
+      $url = preg_replace('#ç#', 'c', $url);
+      $url = preg_replace('#è|é|ê|ë#', 'e', $url);
+      $url = preg_replace('#È|É|Ê|Ë#', 'E', $url);
+      $url = preg_replace('#à|á|â|ã|ä|å#', 'a', $url);
+      $url = preg_replace('#@|À|Á|Â|Ã|Ä|Å#', 'A', $url);
+      $url = preg_replace('#ì|í|î|ï#', 'i', $url);
+      $url = preg_replace('#Ì|Í|Î|Ï#', 'I', $url);
+      $url = preg_replace('#ð|ò|ó|ô|õ|ö#', 'o', $url);
+      $url = preg_replace('#Ò|Ó|Ô|Õ|Ö#', 'O', $url);
+      $url = preg_replace('#ù|ú|û|ü#', 'u', $url);
+      $url = preg_replace('#Ù|Ú|Û|Ü#', 'U', $url);
+      $url = preg_replace('#ý|ÿ#', 'y', $url);
+      $url = preg_replace('#Ý#', 'Y', $url);
+
+      return ($url);
+  }
+
   if(isset($_SESSION['idcon'])){
     $useracc = str_replace(' ','',$_GET['recherche']);
     $tofind = "é";
 		$replac = "e";
-    $str = "ééé";
-    $str = htmlentities($str, ENT_NOQUOTES, 'UTF-8');
-    $str = preg_replace('#\&amp;([A-za-z])(?:acute|cedil|circ|grave|ring|tilde|uml)\;#', '\1', $str);
-    $str = preg_replace('#\&amp;([A-za-z]{2})(?:lig)\;#', '\1', $str);
-    $str = preg_replace('#\&amp;[^;]+\;#', '', $str);
-		$user = strtr($useracc,$tofind,$replac);
+		$user = str_to_noaccent($useracc);
 
     ?>
       <!DOCTYPE html>
