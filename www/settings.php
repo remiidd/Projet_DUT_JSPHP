8dda4540909329d3d775d1e976c87355c68e4a0c
Rémi Debray <remi281099@orange.fr>
2018-11-22 15:52:08 +0100

    22-11-18 15:52:08

--- www/settings.php
+++ www/settings.php
@@ -57,8 +57,7 @@ if((!isset($_SESSION["idcon"]))||($_SESSION["idcon"]!=$_GET["id"])){
         <?php if(isset($_POST["email"])){
           echo "test";
           ?><script>modif_email(<?php echo $_GET["id"]; ?>,<?php echo $_POST["email"]; ?>);</script><?php
-          $url_refresh = "Location:settings.php?id=".$_GET["id"];
-          header($url_refresh);
+          
         } ?>
         <p class="marge">Date de naissance : <?php $naissance = new DateTime($data["naissance"]); echo $naissance->format("d / m / Y"); ?> <a class="modif_info_bouton_naissance"><i class="fas fa-pencil-alt"></i> Modifier</a><i class="txt_modif_naissance">
           <form action="" method="post">
