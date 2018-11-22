6d9ac0c94ec860d3e2b2b7d0bfd4a9e751ac42cc
Rémi Debray <remi281099@orange.fr>
2018-11-22 14:06:43 +0100

    22-11-18 14:06:43

--- www/settings.php
+++ www/settings.php
@@ -77,16 +77,6 @@ if((!isset($_SESSION["idcon"]))||($_SESSION["idcon"]!=$_GET["id"])){
           $url_refresh = "Location:settings.php?id=".$_GET["id"];
           header($url_refresh);
         } ?>
-        <p class="marge">Ville : <?php echo $data["ville"]; ?> <a class="modif_info_bouton_ville"><i class="fas fa-pencil-alt"></i> Modifier</a><i class="txt_modif_ville">
-          <form action="" method="post">
-            <input required type="text" name="ville"/>
-            <input id="inscriBout" type="submit" value="Valider" onclick="modif()"/>
-          </form></i></p>
-        <?php if(isset($_POST["ville"])){
-          $req = $bdd->query('UPDATE profil SET ville=\''.$_POST["ville"].'\' WHERE id=\''.$_GET["id"].'\'');
-          $url_refresh = "Location:settings.php?id=".$_GET["id"];
-          header($url_refresh);
-        } ?>
         <hr>
         <h5><i class="fas fa-cog"></i> Informations personnelles du profil</h5>
         <p class="marge">Photo de profil :</p>
