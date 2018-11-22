8fb145bc6e16e8cec8b9b7915bcc6465ee0cf33f
Rémi Debray <remi281099@orange.fr>
2018-11-22 15:41:36 +0100

    22-11-18 15:41:36

--- www/scripts/php/req.php
+++ www/scripts/php/req.php
@@ -1,6 +1,7 @@
 <?php
-if($_GET["id_req"]==0) {
+echo "salut";
+/*if($_GET["id_req"]==0) {
   $req = $bdd->query('UPDATE profil SET email=\''.$_GET["email"].'\' WHERE id=\''.$_GET["id_profil"].'\'');
   $req->closeCursor();
-}
+}*/
 ?>
