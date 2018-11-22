d2c238d26bedae32349cb9fe9fee05d955578155
aloisguitton <aloisguitton@orange.fr>
2018-11-22 10:49:15 +0100

    aze

--- www/recherche.php
+++ www/recherche.php
@@ -51,6 +51,8 @@
                 $couple2 = strtolower(str_to_noaccent(str_replace(' ', '', $donnees['prenom'] . $donnees['nom'])));
                 if(preg_match("#$user#i", "$couple1") || preg_match("#$user#i", "$couple2")){
                   $trouve = true;
+                  $reponse1 = $bdd-> query("SELECT * FROM amis WHERE id=$_SESSION['idcon'] AND id_amis=$donnees['id']");
+                  echo "ici: " . $reponse1;
                   ?>
                     <div class="rech_user">
                       <div class="rech_div_img">
--- www/scripts/php/ajouter_amis.php
+++ www/scripts/php/ajouter_amis.php
@@ -2,7 +2,6 @@
   session_start();
   if (isset($_SESSION['idcon'])) {
     if(isset($_GET['id'])){
-      echo $_GET['id'] . "  zzz     " . $_GET['id_amis'];
       try{
         $bdd = new PDO('mysql:host=lulipa.server.r-heberg.fr;dbname=derayalois;port=3306;charset=utf8', 'derayalois', 'testdebrayalois');
       }
@@ -25,7 +24,9 @@
         'statut' => "demande"
       ));
 
-      echo "fait";
+      header("Location: ../../index.php");
+      exit()
+      
     }
     else {
       header("Location: ../../index.php");
