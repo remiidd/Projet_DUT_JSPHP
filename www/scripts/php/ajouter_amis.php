76f441766ede6eb477d4dbbc93e15ad3a5872dbc
aloisguitton <aloisguitton@orange.fr>
2018-11-22 10:26:02 +0100

    zae

--- www/scripts/php/ajouter_amis.php
+++ www/scripts/php/ajouter_amis.php
@@ -2,7 +2,7 @@
   session_start();
   if (isset($_SESSION['idcon'])) {
     if(isset($_GET['id'])){
-      echo $_GET['id'] . "       " . $_GET['id_amis']
+      echo $_GET['id'] . "  zzz     " . $_GET['id_amis']
     }
     else {
       echo "pas de donnéee";
