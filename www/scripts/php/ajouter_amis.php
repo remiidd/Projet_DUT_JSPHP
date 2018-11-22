a7af615eced0866e69939c001fe165c1b48cc61a
aloisguitton <aloisguitton@orange.fr>
2018-11-22 10:33:03 +0100

    ajout en base

--- www/scripts/php/ajouter_amis.php
+++ www/scripts/php/ajouter_amis.php
@@ -1,12 +1,33 @@
 <?php
   session_start();
   if (isset($_SESSION['idcon'])) {
-    echo "ici";
     if(isset($_GET['id'])){
       echo $_GET['id'] . "  zzz     " . $_GET['id_amis'];
+      try{
+        $bdd = new PDO('mysql:host=lulipa.server.r-heberg.fr;dbname=derayalois;port=3306;charset=utf8', 'derayalois', 'testdebrayalois');
+      }
+      catch (Exception $e){
+            die('Erreur : ' . $e->getMessage());
+      }
+      $req = $bdd->prepare('INSERT INTO `amis`(`id`, `id_amis`, `status`)
+        VALUES (:id, :id_amis, : status)');
+      $req->execute(array(
+        'id' => $_GET['id'],
+        'id_amis' => $_GET['id_amis'],
+        'status' => "en attente"
+      ));
+
+      $req = $bdd->prepare('INSERT INTO `amis`(`id`, `id_amis`, `status`)
+        VALUES (:id, :id_amis, : status)');
+      $req->execute(array(
+        'id' => $_GET['id_amis'],
+        'id_amis' => $_GET['id'],
+        'status' => "demande"
+      ));
     }
     else {
-      echo "pas de donnéee";
+      header("Location: ../../index.php");
+      exit();
     }
   }
   else {
