d6be07aeaf6e63ff1b2ad696b7847aed8b17305c
aloisguitton <aloisguitton@orange.fr>
2018-11-22 11:14:45 +0100

    azeaze

--- www/recherche.php
+++ www/recherche.php
@@ -55,7 +55,9 @@
                   $trouve = true;
                   $id_amis = $donnees['id'];
                   $reponse1 = $bdd->query("SELECT statut FROM amis WHERE id_amis=$id_amis AND id=$id_user");
-                  
+                  while ($a = $response1->fetch()) {
+                    echo "ici : " . $a['statut'];
+                  }
 
                   ?>
                     <div class="rech_user">
