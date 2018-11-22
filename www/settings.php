a0ecadf0af2f90589e97495ee1d37b153e4901ee
Rémi Debray <remi281099@orange.fr>
2018-11-22 13:41:58 +0100

    22-11-18 13:41:58

--- www/settings.php
+++ www/settings.php
@@ -80,6 +80,7 @@ if((!isset($_SESSION["idcon"]))||($_SESSION["idcon"]!=$_GET["id"])){
           $url_refresh = "Location:settings.php?id=".$_GET["id"];
           $req_naissance = $bdd->query('UPDATE profil SET naissance=\''.$_POST["naissance"].'\' WHERE id=\''.$_GET["id"].'\'');
           $req_naissance->closeCursor();
+          echo "salut";
           header($url_refresh);
         } ?>
         <p class="marge">Ville : <?php echo $data["ville"]; ?> <a class="modif_info_bouton_ville"><i class="fas fa-pencil-alt"></i> Modifier</a><i class="txt_modif_ville"><form action="" method="post"><input required type="text" name="ville"/><input id="inscriBout" type="submit" value="Valider" onclick="modif()"/></form></i></p>
