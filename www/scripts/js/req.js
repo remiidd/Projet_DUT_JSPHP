58522edaf955e7c74c986cd9f44a9ca7cf9cdcd7
Rémi Debray <remi281099@orange.fr>
2018-11-22 15:29:12 +0100

    22-11-18 15:29:12

--- www/scripts/js/req.js
+++ www/scripts/js/req.js
@@ -33,14 +33,15 @@ function liker_post(id_posts, id_profils) {
   }
 }
 
-function modif_email(id_posts, id_profils) {
+function modif_email(id_profils, email) {
   var xhr = getXMLHttpRequest();
 
-  var id_post = encodeURIComponent(id_posts);
+	var id_req = 0;
+  var email = encodeURIComponent(id_posts);
   var id_profil = encodeURIComponent(id_profils);
   console.log("salut");
   try {
-    xhr.open("GET", "scripts/php/like.php?id_post="+id_post+"&id_profil="+id_profil,true);
+    xhr.open("GET", "scripts/php/req.php?id_req="+id_req+"&email="+email+"&id_profil="+id_profil,true);
     xhr.send(null);
   }catch(error) {
     alert(error);
