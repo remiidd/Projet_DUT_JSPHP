2a4f664fe38ab9b84019c89410f4f10331159d90
Rémi Debray <remi281099@orange.fr>
2018-11-22 13:38:45 +0100

    22-11-18 13:38:45

--- www/settings.php
+++ www/settings.php
@@ -77,7 +77,7 @@ if((!isset($_SESSION["idcon"]))||($_SESSION["idcon"]!=$_GET["id"])){
             <input id="inscriBout" type="submit" value="Valider" onclick="modif()"/>
           </form></i></p>
         <?php if(isset($_POST["naissance"])){
-          $req = $bdd->query('UPDATE profil SET naissance=\''.$_POST["naissance"].'\' WHERE id=\''.$_GET["id"].'\'');
+          $req_naissance = $bdd->query('UPDATE profil SET naissance=\''.$_POST["naissance"].'\' WHERE id=\''.$_GET["id"].'\'');
           $url_refresh = "Location:settings.php?id=".$_GET["id"];
           header($url_refresh);
         } ?>
