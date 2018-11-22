bdab83fe1356d66a42105461dd19ef0f740ea8b3
aloisguitton <aloisguitton@orange.fr>
2018-11-22 10:26:30 +0100

    point virgule

--- www/scripts/php/ajouter_amis.php
+++ www/scripts/php/ajouter_amis.php
@@ -1,8 +1,9 @@
 <?php
   session_start();
   if (isset($_SESSION['idcon'])) {
+    echo "ici";
     if(isset($_GET['id'])){
-      echo $_GET['id'] . "  zzz     " . $_GET['id_amis']
+      echo $_GET['id'] . "  zzz     " . $_GET['id_amis'];
     }
     else {
       echo "pas de donnéee";
