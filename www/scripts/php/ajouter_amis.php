2d944f2c699ccd6f80a3338fb33a6d59795048f1
aloisguitton <aloisguitton@orange.fr>
2018-11-22 10:20:54 +0100

    da

--- www/scripts/php/ajouter_amis.php
+++ www/scripts/php/ajouter_amis.php
@@ -1,12 +1,11 @@
 <?php
   session_start();
   if (isset($_SESION['idcon'])) {
-    if(isset($_DATA['id'])){
-      echo $_DATA['id'] . "       " . $_DATA['id_amis'];
+    if(isset($_GET['id'])){
+      echo $_GET['id'] . "       " . $_GET['id_amis']
     }
     else {
-      header("Location: ../../index.php");
-      exit();
+      echo "pas de donnéee";
     }
   }
   else {
