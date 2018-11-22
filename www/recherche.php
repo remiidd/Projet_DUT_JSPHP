1f17cb7ec4d0cd1539014cfd2431c0afce154e1c
aloisguitton <aloisguitton@orange.fr>
2018-11-22 11:00:24 +0100

    sdf

--- www/recherche.php
+++ www/recherche.php
@@ -51,8 +51,7 @@
                 $couple2 = strtolower(str_to_noaccent(str_replace(' ', '', $donnees['prenom'] . $donnees['nom'])));
                 if(preg_match("#$user#i", "$couple1") || preg_match("#$user#i", "$couple2")){
                   $trouve = true;
-                  $id_amis = $_donnees['id'];
-                  $reponse1 = $bdd->query("SELECT * FROM amis WHERE id_amis=$id_amis AND id=$_SESSION['idcon']");
+                  $reponse1 = $bdd->query("SELECT * FROM amis WHERE id_amis=$_donnees['id'] AND id=$_SESSION['idcon']");
 
                   ?>
                     <div class="rech_user">
