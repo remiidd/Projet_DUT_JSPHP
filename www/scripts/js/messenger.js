a632b3629480d706b4e7ed3fe45beb2f14a7f243
aloisguitton <aloisguitton@orange.fr>
2018-11-23 16:39:44 +0100

    azerazer

--- www/scripts/js/messenger.js
+++ www/scripts/js/messenger.js
@@ -1,8 +1,8 @@
 function resize_msg(){
   var his = document.getElementsByClassName("historique");
   var div = document.getElementsByClassName("discution");
-  var taille = document.body.innertHeight - 53;
-  console.log(document.body.innertHeight);
+  var taille = window.innertHeight - 53;
+  console.log(window.innertHeight);
   for (i = 0; i < div.length; i++) {
     div[i].style.height = taille + "px";
   }
