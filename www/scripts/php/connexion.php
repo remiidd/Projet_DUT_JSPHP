b060b41d04a64afc158b5ad851c72a853f919c2e
aloisguitton <aloisguitton@orange.fr>
2018-11-26 22:12:16 +0100

    26/11/2018 22:12:16

--- www/scripts/php/connexion.php
+++ www/scripts/php/connexion.php
@@ -22,7 +22,7 @@
     if(($donnees['password'] == $mdp && $donnees['email'] == $util) || ($donnees['password'] == $mdp && $donnees['numerotel'] == $util)){
       $id = $donnees['id'];
       $_SESSION['idcon'] = $id;
-      header("Location:../../profil.php?id=$id");
+      header("Location:../../profil-$id");
       exit();
       $cook = true;
     }
