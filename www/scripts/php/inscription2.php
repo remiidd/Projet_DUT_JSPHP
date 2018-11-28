1e94c0f288e07a7c08d7e3c96f5046ebf75da1ef
aloisguitton <aloisguitton@orange.fr>
2018-11-28 21:44:33 +0100

    qsd

--- www/scripts/php/inscription2.php
+++ www/scripts/php/inscription2.php
@@ -69,7 +69,7 @@
     $donnees = $reponse->fetch();
 
     $id = $donnees['id'];
-    $_SESSION['idcon'] == $donnees['id'];
+    $_SESSION['idcon'] == "azeaze";
 
     if($existpp || $existcover){
       $target_dir = "../../src/media/profils/";
