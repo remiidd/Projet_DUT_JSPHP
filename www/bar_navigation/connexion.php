ab35a1d5ce40b6654c5e51a83aad8ff891c50336
aloisguitton <aloisguitton@orange.fr>
2018-11-30 10:54:10 +0100

    30/11/2018 10:54:10

--- www/bar_navigation/connexion.php
+++ www/bar_navigation/connexion.php
@@ -1,5 +1,5 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
   <div class="navbar-brand">
-    <a href="index.php" >IUT</a>
+    <a href="index.php" >BananaBook</a>
   </div>
 </nav>
