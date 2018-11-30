0a7228e833fdf708150e0c55050a317dbe8a4062
Rémi Debray <remi281099@orange.fr>
2018-11-30 10:33:26 +0100

    30-11-18 10:33:26

--- www/settings.php
+++ www/settings.php
@@ -49,17 +49,7 @@ if((!isset($_SESSION["idcon"]))||($_SESSION["idcon"]!=$_GET["id"])){
         </div><br/>
         <p class="marge"><a href="profil.php?id=<?php echo $_GET["id"]; ?>">Revenir au profil</a></p>
         <h5><i class="fas fa-cog"></i> Paramètres du compte</h5>
-        <p class="marge">Email : <?php echo $data["email"]); ?> <a class="modif_info_bouton_email"><i class="fas fa-pencil-alt"></i> Modifier</a><i class="txt_modif_email">
-          <form action="" method="post">
-            <input required type="text" name="email"/>
-            <input id="inscriBout" type="submit" value="Valider" onclick="modif()"/>
-          </form></i></p>
-        <?php if(isset($_POST["email"])){
-          $req = $bdd->query('UPDATE profil SET email=\''.$_POST["email"].'\' WHERE id=\''.$_GET["id"].'\'');
-          $req->closeCursor();
-          $url_refresh = "Location:settings.php?id=".$_GET["id"];
-          header($url_refresh);
-        } ?>
+        
         <p class="marge">Date de naissance : <?php $naissance = new DateTime($data["naissance"]); echo $naissance->format("d / m / Y"); ?> <a class="modif_info_bouton_naissance"><i class="fas fa-pencil-alt"></i> Modifier</a><i class="txt_modif_naissance">
           <form action="" method="post">
             <input required type="date" name="naissance"/>
