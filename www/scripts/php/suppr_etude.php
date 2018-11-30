0ddd304fc7bb9668e9b5f4810abd322ca72cce08
Rémi Debray <remi281099@orange.fr>
2018-11-30 11:27:14 +0100

    30-11-18 11:27:13

---
+++ www/scripts/php/suppr_etude.php
@@ -0,0 +1,21 @@
+<?php
+header("Content-Type: text/plain");
+
+if(isset($_GET["id"])) {
+  $id = $_GET["id"];
+} else {
+  $id = null;
+}
+
+if(($id!=null)&&($id!=0)) {
+  $bdd = new PDO('mysql:host=lulipa.server.r-heberg.fr;dbname=derayalois;charset=utf8', 'derayalois', 'testdebrayalois');
+
+    //DELETE FROM `derayalois`.`like_table` WHERE `like_table`.`id` = 8;
+  $req = $bdd->query('DELETE FROM etude WHERE id=\''.$id.'\'');
+}
+
+
+
+
+
+?>
