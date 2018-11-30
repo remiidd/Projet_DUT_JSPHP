d7e16657d5d7f6e75748e2b5791f30b26a40ae2a
Rémi Debray <remi281099@orange.fr>
2018-11-30 15:42:22 +0100

    30-11-18 15:42:22

--- www/settings.php
+++ www/settings.php
@@ -113,7 +113,7 @@ if((!isset($_SESSION["idcon"]))||($_SESSION["idcon"]!=$_GET["id"])){
             if(in_array($extension_uploadpp,$extensions_valides)){
               //TOUT EST OK
               $target_dir = "../../src/media/profils/";
-              $target_file =  $target_dir.$_GET["id"]."-ppA.".$extension_uploadpp;
+              $target_file =  $target_dir.$_GET["id"]."-pp.".$extension_uploadpp;
               $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
               $resultat = move_uploaded_file($_FILES['pp']['tmp_name'],$target_file);
             }
