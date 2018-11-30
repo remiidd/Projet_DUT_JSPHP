4f137c746fedab2e365563fb8f8e2b493f6b9966
Rémi Debray <remi281099@orange.fr>
2018-11-30 13:47:15 +0100

    30-11-18 13:47:15

--- www/bar_navigation/connexion.php
+++ www/bar_navigation/connexion.php
@@ -1,5 +1,5 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
   <div class="navbar-brand">
-    <a href="index.php" >BananaBook</a>
+    <a href="index.php" >BananaBook 🍌</a>
   </div>
 </nav>
--- www/bar_navigation/nonco.php
+++ www/bar_navigation/nonco.php
@@ -3,7 +3,7 @@
     ?>
       <nav class="navbar navbar-expand-lg navbar-light bg-light">
         <div class="navbar-brand">
-          <a href="index.php" >BananaBook</a>
+          <a href="index.php" >BananaBook 🍌</a>
         </div>
 
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -36,7 +36,7 @@
     ?>
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
       <div class="navbar-brand">
-        <a href="index.php" >BananaBook</a>
+        <a href="index.php" >BananaBook 🍌</a>
       </div>
 
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
