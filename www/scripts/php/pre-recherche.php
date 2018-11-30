64553beaf19ba0b1f2d54c61b6c03862900701ca
aloisguitton <aloisguitton@orange.fr>
2018-11-30 13:50:58 +0100

    30/11/2018 13:50:58

--- www/scripts/php/pre-recherche.php
+++ www/scripts/php/pre-recherche.php
@@ -1,5 +1,7 @@
 <?php
-  $rech = strtolower(str_to_noaccent(str_replace(' ','',$_POST['recherche'])));
-  header("Location: /recherche-$rech");
-  exit();
+  if(isset($_POST['recherche'])){
+    $rech = strtolower(str_to_noaccent(str_replace(' ','',$_POST['recherche'])));
+    header("Location: /recherche-$rech");
+    exit();
+  }
 ?>
