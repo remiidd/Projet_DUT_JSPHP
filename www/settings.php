858ec648dc1e8361b168576ca4c86ca3a77c17ab
Rémi Debray <remi281099@orange.fr>
2018-12-04 08:33:33 +0100

    04-12-18 08:33:33

--- www/settings.php
+++ www/settings.php
@@ -107,9 +107,7 @@ if((!isset($_SESSION["idcon"]))||($_SESSION["idcon"]!=$_GET["id"])){
           <?php
           $extensions_valides = array('jpg','jpeg','gif','png');
           if($_FILES['pp']['name']!="") {
-            echo "SALUT MEC";
             $extension_uploadpp = strtolower(substr(strrchr($_FILES['pp']['name'],'.'),1));
-            echo $_FILES['pp']['name'];
             if(in_array($extension_uploadpp,$extensions_valides)){
               //TOUT EST OK
               if($data["photo_profil"]!="") {
