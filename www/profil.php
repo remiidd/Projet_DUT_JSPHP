892a10b592e66413726395c724a641f3217f1a54
Rémi Debray <remi281099@orange.fr>
2018-12-04 08:56:29 +0100

    04-12-18 08:56:29

--- www/profil.php
+++ www/profil.php
@@ -55,7 +55,7 @@
             <tr>
               <td><p>Agé de <strong><?php $today = new DateTime();$naissance = new DateTime($data["naissance"]);echo $today->diff($naissance)->format("%Y");?></strong> ans</p>
               <?php if(($today->format("%m%d"))==($naissance->format("%m%d"))) { ?><p>Bon anniversaire !</p><?php } ?></td>
-              <td><?php if($emploi["travail"]!=""){ ?><p>Etudie à <strong><?php echo $etude["etablissement"]; ?></strong></p><?php } ?></td>
+              <td><?php if($etude["etablissement"]!=""){ ?><p>Etudie à <strong><?php echo $etude["etablissement"]; ?></strong></p><?php } ?></td>
             </tr>
           </table>
         <?php if($_SESSION["idcon"]==$_GET["id"]){ ?>
