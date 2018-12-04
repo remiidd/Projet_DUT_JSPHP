37ecffce563acb1a2d5a2edfa59c28af887e7132
aloisguitton <aloisguitton@orange.fr>
2018-12-04 08:44:53 +0100

    04/12/2018 08:44:52

---
+++ www/scripts/php/messenger.php
@@ -0,0 +1,12 @@
+<?php
+  session_start();
+  if(isset($_GET['amis'])){
+    set_amis($_GET['amis']);
+  }
+
+  function set_amis($amis){
+    $_SESSION('amis_conv') == $amis;
+    header("Location: /messenger");
+    exit();
+  }
+?>
