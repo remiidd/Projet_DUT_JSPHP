f0c9eed5ecab01aa5252961a7117fb06db345b1b
aloisguitton <aloisguitton@orange.fr>
2018-12-04 09:44:52 +0100

    04/12/2018 09:44:52

--- www/messenger.php
+++ www/messenger.php
@@ -86,27 +86,31 @@
             <div class="discution">
               <div class="message">
                 <?php
-                /*  $moi = $_SESSION['idcon'];
-                  try{
-                    $bdd = new PDO('mysql:host=lulipa.server.r-heberg.fr;dbname=derayalois;port=3306;charset=utf8', 'derayalois', 'testdebrayalois');
-                  }
-                  catch (Exception $e){
-                        die('Erreur : ' . $e->getMessage());
-                  }
-                  $reponse = $bdd->query("SELECT id_exp, id_dest, message FROM message WHERE id_exp=".$moi." OR id_dest=".$moi." order by id ASC LIMIT 15");
+                  if(isset($_SESSION['amis_conv'])){
+                    $moi = $_SESSION['idcon'];
+                    $lui = $_SESSION['amis_conv'];
+                    try{
+                      $bdd = new PDO('mysql:host=lulipa.server.r-heberg.fr;dbname=derayalois;port=3306;charset=utf8', 'derayalois', 'testdebrayalois');
+                    }
+                    catch (Exception $e){
+                          die('Erreur : ' . $e->getMessage());
+                    }
+                    $reponse = $bdd->query("SELECT id_exp, id_dest, message FROM message WHERE id_exp=".$moi." AND id_dest=".$lui." OR id_dest=".$moi." AND id_exp=".$lui." order by id ASC LIMIT 15");
 
-                  while ($donnees = $reponse->fetch()){
-                    if ($donnees['id_exp'] == $_SESSION['idcon']) {
-                      echo "<div class=\"bulle-moi\">
-                              <p>".$donnees['message']."</p>
-                            </div>";
+                    while ($donnees = $reponse->fetch()){
+                      if ($donnees['id_exp'] == $_SESSION['idcon']) {
+                        echo "<div class=\"bulle-moi\">
+                                <p>".$donnees['message']."</p>
+                              </div>";
+                      }
+                      else{
+                        echo "<div class=\"bulle-ami\">
+                                <p>".$donnees['message']."</p>
+                              </div>";
+                      }
                     }
-                    else{
-                      echo "<div class=\"bulle-ami\">
-                              <p>".$donnees['message']."</p>
-                            </div>";
-                    }
-                  }*/
+                  }
+
                  ?>
               </div>
               <div class="zone_message">
