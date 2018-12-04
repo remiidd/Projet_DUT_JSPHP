4977d4fb90ea6e6faf1bb142947c46d8916f3edd
aloisguitton <aloisguitton@orange.fr>
2018-12-04 10:01:35 +0100

    04/12/2018 10:01:35

--- www/scripts/js/messenger.js
+++ www/scripts/js/messenger.js
@@ -10,3 +10,23 @@ function resize_msg(){
     his[i].style.height = taille + "px";
   }
 }
+
+function charger(){
+
+    setTimeout( function(){
+        // on lance une requête AJAX
+        $.ajax({
+            url : "charger.php",
+            type : GET,
+            success : function(html){
+                $('#messages').prepend(html); // on veut ajouter les nouveaux messages au début du bloc #messages
+            }
+        });
+
+        charger(); // on relance la fonction
+
+    }, 5000); // on exécute le chargement toutes les 5 secondes
+
+}
+
+charger();
