1e33a1223285840b6cff499a28be7d6143c50127
aloisguitton <aloisguitton@orange.fr>
2018-12-04 09:55:08 +0100

    04/12/2018 09:55:07

--- www/scripts/php/send_message.php
+++ www/scripts/php/send_message.php
@@ -1,6 +1,22 @@
 <?php
   session_start();
   if (isset($_SESSION['idcon']) && isset($_POST['message']) && isset($_SESSION['amis_conv'])) {
-    echo $_POST['message'];
+    try{
+      $bdd = new PDO('mysql:host=lulipa.server.r-heberg.fr;dbname=derayalois;port=3306;charset=utf8', 'derayalois', 'testdebrayalois');
+    }
+    catch (Exception $e){
+          die('Erreur : ' . $e->getMessage());
+    }
+
+    $req = $bdd->prepare('INSERT INTO `message`(`id`, `id_exp`, `id_dest`, `message`)
+      VALUES (:id, :id_exp, :id_dest, :message)');
+    $req->execute(array(
+      'id' => NULL,
+      'id_exp' => $_SESSION['idcon'],
+      'id_dest' => $_SESSION['amis_conv'],
+      'message' => $_POST['message']
+    ));
   }
+  header("Location: /messenger");
+  exit();
 ?>
