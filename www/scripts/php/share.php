347ed32f9b8879735c88cb7ff40e11bc49ce7791
Rémi Debray <remi281099@orange.fr>
2018-12-04 10:01:07 +0100

    04-12-18 10:01:07

--- www/scripts/php/share.php
+++ www/scripts/php/share.php
@@ -18,7 +18,15 @@ if(($id_post!=null)&&($id_profil!=null)&&($id_post!=0)&&($id_profil!=0)) {
   $data = $req->fetch();
 
   echo "id post = ".$id_post." id profil = ".$id_profil;
-  $req = $bdd->query('INSERT INTO like_table(id, id_post, profil_like) VALUES(NULL, \''.$id_post.'\', \''.$id_profil.'\')');
+
+  $content = md5("share").".$id_post";
+  $req = $bdd->prepare('INSERT INTO posts(id, nom_createur, date_publication, contenu, photo, profil, nb_com, nb_like, nb_share) VALUES(NULL, :noms, CURRENT_DATE(), :contenu, :photo, :profil,\'0\',\'0\',\'0\')');
+  $req->execute(array(
+    'noms' => $data["prenom"]." ".$data["nom"],
+    'contenu' => $content,
+    'photo' => "",
+    'profil' => $_GET["id_profil"]
+  ));
   //UPDATE `derayalois`.`posts` SET `nb_like` = '1' WHERE `posts`.`id` = 3;
   $req = $bdd->query('UPDATE posts SET nb_share=nb_share+1 WHERE id=\''.$id_post.'\'');
 }
