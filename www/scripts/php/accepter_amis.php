684c2026f8110867b98035f317e09d99a216cd71
aloisguitton <aloisguitton@orange.fr>
2018-12-07 12:14:41 +0100

    07/12/2018 12:14:41

--- www/scripts/php/accepter_amis.php
+++ www/scripts/php/accepter_amis.php
@@ -1,15 +1,9 @@
 <?php
   session_start();
   if(isset($_SESSION['idcon']) && isset($_GET['id_amis'])){
-    $moi = $_SESSION['idcon'];
-    $lui = $_GET['id_amis'];
-    try{
-      $bdd = new PDO('mysql:host=lulipa.server.r-heberg.fr;dbname=derayalois;port=3306;charset=utf8', 'derayalois', 'testdebrayalois');
-    }
-    catch (Exception $e){
-          die('Erreur : ' . $e->getMessage());
-    }
     echo "fait";
+    
+  }
   else {
     header("Location: accueil");
     exit();
