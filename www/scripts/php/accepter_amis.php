b5a75846420ea627b8f447c42d91a571db5a9b1e
aloisguitton <aloisguitton@orange.fr>
2018-12-07 12:15:04 +0100

    07/12/2018 12:15:04

--- www/scripts/php/accepter_amis.php
+++ www/scripts/php/accepter_amis.php
@@ -2,7 +2,10 @@
   session_start();
   if(isset($_SESSION['idcon']) && isset($_GET['id_amis'])){
     echo "fait";
+    $moi = $_SESSION['idcon'];
+    $lui = $_GET['id_amis'];
     
+
   }
   else {
     header("Location: accueil");
