b8e1819dbdd0407467b69015c22169f56a925783
aloisguitton <aloisguitton@orange.fr>
2018-12-07 13:02:01 +0100

    07/12/2018 13:02:00

--- www/scripts/php/accepter_amis.php
+++ www/scripts/php/accepter_amis.php
@@ -11,8 +11,6 @@
           die('Erreur : ' . $e->getMessage());
     }
     $req = $bdd->query("UPDATE amis SET statut=\"amis\" WHERE (id=$moi AND id_amis=$lui) OR (id_amis=$moi AND id=$lui)");
-    echo $moi ." ".$lui;
-
     header("Location: profil-$lui");
     exit();
   }
