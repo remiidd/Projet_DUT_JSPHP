15b66701ae91203165c0f6be5cc57e7179bfd732
Rémi Debray <remi281099@orange.fr>
2018-12-07 13:29:35 +0100

    07-12-18 13:29:35

--- www/scripts/js/comments.js
+++ www/scripts/js/comments.js
@@ -1,9 +1,22 @@
 document.ready(function(){
-  function charger(){
+  function getParameterByName(name) {
+    name = name.replace(/[[]/, [).replace(/[]]/, ]);
+    var regexS = [?&] + name + =([^&#]*);
+    var regex = new RegExp(regexS);
+    var results = regex.exec(window.location.search);
+    if(results == null) {
+      return;
+    } else {
+      return decodeURIComponent(results[1].replace(/+/g, ));
+    }
+  }
 
+
+  function charger(){
+      var id_post = getParameterByName(id);
       setTimeout( function(){
           $.ajax({
-              url : "scripts/php/load_com.php",
+              url : "scripts/php/load_com.php?id="+id_post,
               type : "GET",
               success : function(html){
                   $('#message').html(html);
