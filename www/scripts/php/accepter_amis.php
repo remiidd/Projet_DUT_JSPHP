080c4da395e0c9f3a4eff0f26ad48375955a20db
aloisguitton <aloisguitton@orange.fr>
2018-12-07 12:10:25 +0100

    07/12/2018 12:10:25

--- www/scripts/php/accepter_amis.php
+++ www/scripts/php/accepter_amis.php
@@ -10,8 +10,5 @@
     }
     $req = $bdd->query("UPDATE `amis` SET `statut`=\"amis\" WHERE (`id`=$moi AND `id_amis` = $lui) OR (`id`=$lui AND `id_amis` = $moi)";
   }
-  else {
-    header("Location: accueil");
-    exit();
-  }
+
 ?>
