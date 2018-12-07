f86eeb127dacea37fe53a69b2cf568240c3e200f
aloisguitton <aloisguitton@orange.fr>
2018-12-07 12:13:10 +0100

    07/12/2018 12:13:10

--- www/scripts/php/accepter_amis.php
+++ www/scripts/php/accepter_amis.php
@@ -1,16 +1,15 @@
 <?php
-  if(isset($_SESSION['idcon']) ){
-    if (isset($_GET['id'])) {
-      $moi = $_SESSION['idcon'];
-      $lui = $_GET['id'];
-      try{
-        $bdd = new PDO('mysql:host=lulipa.server.r-heberg.fr;dbname=derayalois;port=3306;charset=utf8', 'derayalois', 'testdebrayalois');
-      }
-      catch (Exception $e){
-            die('Erreur : ' . $e->getMessage());
-      }
-      $req = $bdd->query("UPDATE `amis` SET `statut`=\"amis\" WHERE (`id`=$moi AND `id_amis` = $lui) OR (`id`=$lui AND `id_amis` = $moi)";
+  session_start();
+  if(isset($_SESSION['idcon']) && isset($_GET['id_amis'])){
+    $moi = $_SESSION['idcon'];
+    $lui = $_GET['id_amis'];
+    try{
+      $bdd = new PDO('mysql:host=lulipa.server.r-heberg.fr;dbname=derayalois;port=3306;charset=utf8', 'derayalois', 'testdebrayalois');
     }
+    catch (Exception $e){
+          die('Erreur : ' . $e->getMessage());
+    }
+    $req = $bdd->query("UPDATE `amis` SET `statut`=\"amis\" WHERE (`id`=$moi AND `id_amis` = $lui) OR (`id`=$lui AND `id_amis` = $moi)";
   }
   else {
     header("Location: accueil");
