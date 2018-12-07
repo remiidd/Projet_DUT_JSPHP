8d3dbe84d8c37a39b6641f2f17a98b0e16d69517
Rémi Debray <remi281099@orange.fr>
2018-12-07 12:13:47 +0100

    07-12-18 12:13:47

--- www/scripts/js/comments.js
+++ www/scripts/js/comments.js
@@ -1,4 +1,19 @@
 document.ready(function(){
-  
+  function charger(){
+
+      setTimeout( function(){
+          $.ajax({
+              url : "scripts/php/load_com.php",
+              type : "GET",
+              success : function(html){
+                  $('#message').html(html);
+              }
+          });
+
+          charger();
+
+      }, 3000);
+
+  }
 
 });
