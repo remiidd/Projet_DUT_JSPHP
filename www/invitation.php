b9fed402d33a30be5cd3a1cfc2b92a384af6a7bd
aloisguitton <aloisguitton@orange.fr>
2018-12-07 10:25:38 +0100

    07/12/2018 10:25:38

--- www/invitation.php
+++ www/invitation.php
@@ -1,3 +1,25 @@
 <?php
-  
+  session_start();
+
+  if(isset($_SESSION['idcon'])){
+    ?>
+      <!DOCTYPE html>
+      <html lang="en" dir="ltr">
+        <head>
+          <meta charset="utf-8">
+          <title>Invitation</title>
+          <?php include 'scripts/html/head.html'; ?>
+        </head>
+        <body>
+          <?php include 'bar_navigation/nonco.php'?>
+          <div class="content">
+
+        </body>
+      </html>
+    <?php
+  }
+  else {
+    header("Location: accueil");
+    exit();
+  }
 ?>
