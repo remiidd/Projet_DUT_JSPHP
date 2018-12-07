2567001bba3797925bdc5f207faad3e962baf927
aloisguitton <aloisguitton@orange.fr>
2018-12-07 11:58:21 +0100

    07/12/2018 11:58:21

---
+++ www/scripts/php/accepter_amis.php
@@ -0,0 +1,17 @@
+<?php
+  if(isset($_SESSION['idcon']) && isset($_GET['id'])){
+    $moi = $_SESSION['idcon'];
+    $lui = $_GET['id'];
+    try{
+      $bdd = new PDO('mysql:host=lulipa.server.r-heberg.fr;dbname=derayalois;port=3306;charset=utf8', 'derayalois', 'testdebrayalois');
+    }
+    catch (Exception $e){
+          die('Erreur : ' . $e->getMessage());
+    }
+    $req = $bdd->query("UPDATE `amis` SET `statut`=\"amis\" WHERE (`id`=$moi AND `id_amis` = $lui) OR (`id`=$lui AND `id_amis` = $moi)"
+  }
+  else {
+    header("Location: accueil");
+    exit();
+  }
+?>
