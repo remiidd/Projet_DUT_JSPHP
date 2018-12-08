3fb22b0f2a1c6197bef2c4af04be4d3a093aa9db
aloisguitton <aloisguitton@orange.fr>
2018-12-08 13:22:59 +0100

    08/12/2018 13:22:58

--- www/messenger.php
+++ www/messenger.php
@@ -54,6 +54,7 @@
 
                 for($i = array_pop(array_keys($histo)); $i>=array_shift(array_keys($histo)); $i--){
                   echo $histo[29][1];
+                  echo $i;
                   if(isset($histo[$i])){
                     ?>
                       <a class="histo_perso_href" href="scripts/php/messenger.php?amis=<?php echo $histo[$i][0]?>">
