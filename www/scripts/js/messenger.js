046ffd6822ea21031d933d1c81023401519f03c6
aloisguitton <aloisguitton@orange.fr>
2018-12-08 13:04:56 +0100

    08/12/2018 13:04:56

--- www/scripts/js/messenger.js
+++ www/scripts/js/messenger.js
@@ -1,5 +1,5 @@
 function resize_msg(){
-  var his = document.getElementsByClassName("historique");
+  var his = document.getElementsByID("historique");
   var div = document.getElementsByClassName("discution");
   var taille = window.innerHeight - 56;
   for (i = 0; i < div.length; i++) {
