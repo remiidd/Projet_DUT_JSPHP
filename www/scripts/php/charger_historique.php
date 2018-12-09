7c6066d1a08487ed9bd28ae9826db56a045c24c0
aloisguitton <aloisguitton@orange.fr>
2018-12-09 14:00:50 +0100

    09/12/2018 14:00:50

--- www/scripts/php/charger_historique.php
+++ www/scripts/php/charger_historique.php
@@ -1,7 +1,7 @@
 <?php
   session_start();
   $moi = $_SESSION['idcon'];
-  $mess="";
+  $mess="message";
     try{
       $bdd = new PDO('mysql:host=lulipa.server.r-heberg.fr;dbname=derayalois;port=3306;charset=utf8', 'derayalois', 'testdebrayalois');
     }
