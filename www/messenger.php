fffd2e2c72afb3148d96937f96ba53ec01229a4c
aloisguitton <aloisguitton@orange.fr>
2018-12-09 13:37:06 +0100

    09/12/2018 13:37:06

--- www/messenger.php
+++ www/messenger.php
@@ -61,15 +61,18 @@
                 }
 
                 if(isset($_SESSION['nv_conv'])){
-                /*  for($i = max(array_keys($histo)); $i>=array_shift(array_keys($histo)); $i--){
-                      if($histo[$i][0] == $_SESSION['nv_conv']){
-                        $_SESSION['amis_conv'] = $_SESSION['nv_conv'];
-                        $nouv_conv = false;
-                      }
-                      else {
-                        $_SESSION['amis_conv'] = $_SESSION['nv_conv'];
-                      }
-                  }*/
+                  if(isset($histo)){
+                    for($i = max(array_keys($histo)); $i>=array_shift(array_keys($histo)); $i--){
+                        if($histo[$i][0] == $_SESSION['nv_conv']){
+                          $_SESSION['amis_conv'] = $_SESSION['nv_conv'];
+                          $nouv_conv = false;
+                        }
+                        else {
+                          $_SESSION['amis_conv'] = $_SESSION['nv_conv'];
+                        }
+                    }
+                  }
+
 
                   if($nouv_conv == true){
                     $id_nouveau = $_SESSION['nv_conv'];
