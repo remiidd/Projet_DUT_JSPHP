1a21d58701cb2715165214f0445e4425d2577a5f
Rémi Debray <remi281099@orange.fr>
2018-12-11 17:34:47 +0100

    11-12-18 17:34:47

--- www/scripts/js/feed.js
+++ www/scripts/js/feed.js
@@ -2,10 +2,10 @@ function verif_bar() {
   setTimeout( function(){
       console.log("salut");
       console.log($(document).height() - $(window).height() - $(window).scrollTop());
-      if (($(document).height() - $(window).height() - $(window).scrollTop()) == 0) {
+      if(($(document).height() - $(window).height() - $(window).scrollTop()) == 0) {
          console.log("en bas");
       }
-  }, 3000);
+  }, 30000);
 
   verif_bar();
 
