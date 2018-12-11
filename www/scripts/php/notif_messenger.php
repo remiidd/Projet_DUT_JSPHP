9c18ac447e384f609294c59b0cfaf844950a5035
aloisguitton <aloisguitton@orange.fr>
2018-12-11 15:34:36 +0100

    11/12/2018 15:34:35

--- www/scripts/php/notif_messenger.php
+++ www/scripts/php/notif_messenger.php
@@ -1,5 +1,6 @@
 <?php
   session_start();
+  $mess="<i class=\"fas fa-comments\"></i>";
   try{
     $bdd = new PDO('mysql:host=lulipa.server.r-heberg.fr;dbname=derayalois;port=3306;charset=utf8', 'derayalois', 'testdebrayalois');
   }
@@ -12,6 +13,7 @@
                           WHERE id_dest=$moi AND vu=0");
   $donnees = $reponse->fetch();
   if($donnees['notif']!= 0){
-    echo "848";
+    $mess.="<span id=\"notification_count_menu\">".$donnees['notif']."</span>";
   }
+  echo $mess;
 ?>
