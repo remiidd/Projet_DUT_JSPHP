a22764ebd2c852ea27975b3983f3163bcbd53d8f
aloisguitton <aloisguitton@orange.fr>
2018-12-11 14:05:52 +0100

    11/12/2018 14:05:51

---
+++ www/scripts/php/notif_messenger.php
@@ -0,0 +1,19 @@
+<?php
+  session_start();
+  $mess="";
+  try{
+    $bdd = new PDO('mysql:host=lulipa.server.r-heberg.fr;dbname=derayalois;port=3306;charset=utf8', 'derayalois', 'testdebrayalois');
+  }
+  catch (Exception $e){
+        die('Erreur : ' . $e->getMessage());
+  }
+  $moi = $_SESSION['idcon'];
+  $reponse = $bdd->query("SELECT COUNT(*) as notif
+                          FROM message
+                          WHERE id_dest=$moi AND vu=0");
+  $donnees = $reponse->fetch();
+  if($donnees['notif']!= 0){
+    $mess.='<span id="notification_count_menu">'.$donnees['notif'].'</span>';
+  }
+  echo $mess;
+?>
