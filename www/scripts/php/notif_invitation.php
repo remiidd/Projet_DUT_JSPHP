b44f21949fdd831f5c1828b3fefd794186364e24
aloisguitton <aloisguitton@orange.fr>
2018-12-11 15:07:13 +0100

    11/12/2018 15:07:13

---
+++ www/scripts/php/notif_invitation.php
@@ -0,0 +1,15 @@
+<?php
+  session_start();
+  try{
+    $bdd = new PDO('mysql:host=lulipa.server.r-heberg.fr;dbname=derayalois;port=3306;charset=utf8', 'derayalois', 'testdebrayalois');
+  }
+  catch (Exception $e){
+        die('Erreur : ' . $e->getMessage());
+  }
+  $moi = $_SESSION['idcon'];
+  $reponse = $bdd->query("SELECT COUNT(*) as nb_demande FROM amis WHERE `id`=$moi AND statut=\"demande\"");
+  $donnees = $reponse->fetch();
+  if($donnees['notif']!= 0){
+    echo $donnees['notif'];
+  }
+?>
