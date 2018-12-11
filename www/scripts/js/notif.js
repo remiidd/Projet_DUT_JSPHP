ef6190fe11c2ee11619eefd338ae778ae1b3caae
aloisguitton <aloisguitton@orange.fr>
2018-12-11 14:10:07 +0100

    11/12/2018 14:10:06

---
+++ www/scripts/js/notif.js
@@ -0,0 +1,18 @@
+function notif_messenger(){
+
+    setTimeout( function(){
+        $.ajax({
+            url : "scripts/php/notif_messenger.php",
+            type : "GET",
+            success : function(html){
+                $('#notification_count_menu').InnerHTML(html);
+            }
+        });
+
+        notif_messenger();
+
+    }, 3000);
+
+}
+
+notif_messenger();
