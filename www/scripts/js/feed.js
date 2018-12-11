438ac462346846584c1375adfd760532819beebd
Rémi Debray <remi281099@orange.fr>
2018-12-11 17:15:24 +0100

    11-12-18 17:15:24

--- www/scripts/js/feed.js
+++ www/scripts/js/feed.js
@@ -1,7 +1,13 @@
-setTimeout( function(){
-    console.log("salut");
-    console.log((this.scrollTop + this.clientHeight - this.scrollHeight));
-    if ((this.scrollTop + this.clientHeight - this.scrollHeight) == 0) {
-       console.log("en bas");
-    }
-}, 3000);
+console.log("test");
+
+verif_bar();
+
+function verif_bar() {
+  setTimeout( function(){
+      console.log("salut");
+      console.log((this.scrollTop + this.clientHeight - this.scrollHeight));
+      if ((this.scrollTop + this.clientHeight - this.scrollHeight) == 0) {
+         console.log("en bas");
+      }
+  }, 3000);
+}
