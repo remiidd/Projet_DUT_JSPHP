5347fef1a3ceeb465fec4fed931fdaa8b0cf0b35
aloisguitton <aloisguitton@orange.fr>
2018-12-11 17:14:10 +0100

    11/12/2018 17:14:10

--- www/scripts/php/confirmation.php
+++ www/scripts/php/confirmation.php
@@ -1,3 +1,16 @@
 <?php
-  echo $_GET['code'];
+  $code =  $_GET['code'];
+  try{
+    $bdd = new PDO('mysql:host=lulipa.server.r-heberg.fr;dbname=derayalois;port=3306;charset=utf8', 'derayalois', 'testdebrayalois');
+  }
+  catch (Exception $e){
+        die('Erreur : ' . $e->getMessage());
+  }
+
+  $bdd->exec("UPDATE profil SET verif_email=1 WHERE verif_email=$code");
+
+  echo "<script>
+          alert(\"Votre compte à été validé, tu peux te connecter\");
+          window.location.href='/connexion';
+        </script>" ;
 ?>
