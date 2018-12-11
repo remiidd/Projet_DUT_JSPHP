49b2eee1a24b9ca37e8c8d4b794816e110768141
aloisguitton <aloisguitton@orange.fr>
2018-12-11 16:33:38 +0100

    11/12/2018 16:33:38

--- www/scripts/js/inscription.js
+++ www/scripts/js/inscription.js
@@ -17,8 +17,8 @@ function emailexiste(){
 }
 
 function regemail(){
-  var re = /^(([^<>()[]\.,;:s@"]+(.[^<>()[]\.,;:s@"]+)*)|(".+"))@(([[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}])|(([a-zA-Z-0-9]+.)+[a-zA-Z]{2,}))$/igm;
-  if (re.test(email.value)) {
+  var re = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+  if (re.testemail.value)) {
     console.log("ok");
   }
   else {
