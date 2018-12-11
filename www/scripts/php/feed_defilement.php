3739777c6891f1ee2aea797d718a9ffd9b6f4afe
Rémi Debray <remi281099@orange.fr>
2018-12-11 15:16:46 +0100

    11-12-18 15:16:46

--- www/scripts/php/feed_defilement.php
+++ www/scripts/php/feed_defilement.php
@@ -75,7 +75,7 @@ while($feed = $reponse->fetch()) {
 
 
 <div><hr>
-  <h5><a class="no_deco_link" href="/accueil"><img class="pp_posts" src="<?php if($data["photo_profil"]!=null) { echo $data["photo_profil"]; } else { ?>src/media/default_profil_picture.jpg<?php } ?>" alt="Default profil cover"/><?php echo " ".$feed["nom_createur"]; ?></h5></a><p>
+  <h5><a class="no_deco_link" href="/accueil"><img class="pp_posts" src="src/media/sponso.gif" alt="Sponsophoto"/> Sponsorisé</h5></a><p>
     <i><?php $d_publi = new DateTime($feed["date_publication"]); echo "Le ".$d_publi->format("d/m/Y") ?></i></p><br>
     <p><?php
     $contenu = explode(".",$feed["contenu"]);
