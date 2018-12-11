7c820833ac16a5528bbf7dd59f3c38ec9d6ed564
Rémi Debray <remi281099@orange.fr>
2018-12-11 17:14:29 +0100

    11-12-18 17:14:28

--- www/scripts/js/feed.js
+++ www/scripts/js/feed.js
@@ -1,4 +1,11 @@
-console.log((this.scrollTop + this.clientHeight - this.scrollHeight));
-if ((this.scrollTop + this.clientHeight - this.scrollHeight) == 0) {
-   console.log("en bas");
-}
+
+
+
+
+setTimeout( function(){
+    console.log("salut");
+    console.log((this.scrollTop + this.clientHeight - this.scrollHeight));
+    if ((this.scrollTop + this.clientHeight - this.scrollHeight) == 0) {
+       console.log("en bas");
+    }
+}, 3000);
