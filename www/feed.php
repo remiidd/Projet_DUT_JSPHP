eb9bbd63e3a15359406f0e6612d3ce4bc517aa2f
Rémi Debray <remi281099@orange.fr>
2018-12-11 14:52:02 +0100

    11-12-18 14:52:02

--- www/feed.php
+++ www/feed.php
@@ -3,7 +3,7 @@
   <head>
     <meta charset="utf-8">
     <title>Bananafeed</title>
-
+    <link rel="icon" href="src/img/banana.ico" />
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <!-- <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
     -->
