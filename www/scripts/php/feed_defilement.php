4deed12d1bf8ca221c1e128f9c06c76f6b02dd18
Rémi Debray <remi281099@orange.fr>
2018-12-11 16:46:50 +0100

    11-12-18 16:46:50

--- www/scripts/php/feed_defilement.php
+++ www/scripts/php/feed_defilement.php
@@ -15,6 +15,7 @@ $nb_post = $bdd->query('SELECT COUNT(*)
                         FROM posts
                         WHERE profil IN ('.$liste_amis.')');
 $nb_post = $nb_post->fetch();
+echo $nb_post;
 $reponse = $bdd->query('SELECT *
                         FROM posts
                         WHERE profil IN ('.$liste_amis.')
