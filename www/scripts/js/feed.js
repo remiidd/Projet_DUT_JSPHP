14e610c5680bb65acb7ead20296ba10b6b2a9e1a
Rémi Debray <remi281099@orange.fr>
2018-12-11 17:47:28 +0100

    11-12-18 17:47:28

--- www/scripts/js/feed.js
+++ www/scripts/js/feed.js
@@ -1,9 +1,7 @@
 function verif_bar() {
-  console.log("salut");
   var bottom = $(document).height() - $(window).height() - $(window).scrollTop();
-    console.log(bottom);
     if(bottom == 0) {
-        console.log("en bas");
+        //en bas
     }
 }
 
