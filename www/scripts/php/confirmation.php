b5c6bb11222beef5e9f20e93066995dbf2a61652
aloisguitton <aloisguitton@orange.fr>
2018-12-11 17:24:06 +0100

    11/12/2018 17:24:05

--- www/scripts/php/confirmation.php
+++ www/scripts/php/confirmation.php
@@ -6,11 +6,14 @@
   catch (Exception $e){
         die('Erreur : ' . $e->getMessage());
   }
+  $reponse = $bdd->query("SELECT id FROM profil WHERE verif_email=".$code);
+  $donnees = $reponse->fetch();
+  echo $donnees['id'];
+  //$bdd->exec("UPDATE profil SET verif_email=NULL WHERE verif_email=".$code);
 
-  $bdd->exec("UPDATE profil SET verif_email=NULL WHERE verif_email=".$code);
 
-  echo "<script>
+/*  echo "<script>
           alert(\"Votre compte à été validé, tu peux te connecter\");
           window.location.href='/connexion';
-        </script>" ;
+        </script>" ;*/
 ?>
