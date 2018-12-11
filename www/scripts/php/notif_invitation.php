ea12d72d4a84ddf2db6e40408097f0038dbd1aee
aloisguitton <aloisguitton@orange.fr>
2018-12-11 15:31:27 +0100

    11/12/2018 15:31:27

--- www/scripts/php/notif_invitation.php
+++ www/scripts/php/notif_invitation.php
@@ -10,7 +10,7 @@
   $moi = $_SESSION['idcon'];
   $reponse = $bdd->query("SELECT COUNT(*) as nb_demande FROM amis WHERE `id`=$moi AND statut=\"demande\"");
   $donnees = $reponse->fetch();
-  if($donnees['notif']!= 0){
+  if($donnees['nb_demande']!= 0){
     echo $donnees['nb_demande'];
   }
 ?>
