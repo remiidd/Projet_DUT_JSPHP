5a30162a6b005654d993145eec298cb57b7245ae
Rémi Debray <remi281099@orange.fr>
2018-12-11 15:12:15 +0100

    11-12-18 15:12:15

--- www/scripts/php/feed_defilement.php
+++ www/scripts/php/feed_defilement.php
@@ -65,3 +65,66 @@ while($feed = $reponse->fetch()) {
     </ul>
   </div><?php
 } ?><hr>
+
+
+
+
+
+
+
+
+
+<div><hr>
+  <h5><a class="no_deco_link" href="/accueil"><img class="pp_posts" src="<?php if($data["photo_profil"]!=null) { echo $data["photo_profil"]; } else { ?>src/media/default_profil_picture.jpg<?php } ?>" alt="Default profil cover"/><?php echo " ".$feed["nom_createur"]; ?></h5></a><p>
+    <i><?php $d_publi = new DateTime($feed["date_publication"]); echo "Le ".$d_publi->format("d/m/Y") ?></i></p><br>
+    <p><?php
+    $contenu = explode(".",$feed["contenu"]);
+    if($contenu[0]==md5("share")){
+      ?>
+        <div class="partage">
+          <?php
+            $res = $bdd->query('SELECT * FROM posts WHERE id=\''.$contenu[1].'\'');
+            $post_share = $res->fetch();
+            $res = $bdd->query('SELECT * FROM profil WHERE id=\''.$post_share["profil"].'\'');
+            $profil_share = $res->fetch();
+          ?>
+          <div><hr>
+            <h5><a class="no_deco_link" href="<?php echo "profil-".$post_share["profil"]; ?>"><img class="pp_posts" src="<?php if($profil_share["photo_profil"]!=null) { echo $profil_share["photo_profil"]; } else { ?>src/media/default_profil_picture.jpg<?php } ?>" alt="Default profil cover"/><?php echo " ".$post_share["nom_createur"]; ?></h5></a><p>
+              <i><?php $d_publi = new DateTime($post_share["date_publication"]); echo "Le ".$d_publi->format("d/m/Y") ?></i></p><br>
+              <p><?php
+              $contenu2 = explode(".",$post_share["contenu"]);
+              if($contenu2[0]==md5("share")){
+                ?>
+                  <div class="partage">
+                    <?php
+                      $req = $bdd->query('SELECT * FROM posts WHERE id=\''.$contenu2[1].'\'');
+                      $post_share2 = $req->fetch();
+                      $req = $bdd->query('SELECT * FROM profil WHERE id=\''.$post_share2["profil"].'\'');
+                      $profil_share2 = $req->fetch();
+                    ?>
+                    <h5><a class="no_deco_link" href="<?php echo "profil-".$post_share2["profil"]; ?>"><img class="pp_posts" src="<?php if($profil_share2["photo_profil"]!=null) { echo $profil_share2["photo_profil"]; } else { ?>src/media/default_profil_picture.jpg<?php } ?>" alt="Default profil cover"/><?php echo " ".$post_share2["nom_createur"]; ?></h5></a><p>
+                      <i><?php $d_publi = new DateTime($post_share2["date_publication"]); echo "Le ".$d_publi->format("d/m/Y") ?></i></p><br>
+                    <a href="<?php echo "posts.php?id=".$contenu2[1]; ?>">Liens vers la publication de <?php echo $post_share2["nom_createur"]; ?></a>
+                  </div>
+                <?php
+              } else {
+                echo $post_share["contenu"];
+              } ?></p><br>
+          </div>
+        </div>
+      <?php
+    } else {
+      echo $feed["contenu"];
+    } ?></p><br>
+  <ul class="barre_posts">
+    <?php
+    $okk = false;
+    $result = $bdd->query('SELECT * FROM like_table WHERE profil_like=\''.$_SESSION["idcon"].'\' AND id_post=\''.$feed["id"].'\'');
+    if($result->fetch()){$okk=true;}
+    ?>
+    <li class="elements_barre_posts"><a href="" onclick="liker_post(<?php echo $feed["id"].",".$_SESSION["idcon"]; ?>)"><?php echo $feed["nb_like"];?> Likes <i class="<?php if($okk){ echo "fas"; } else { echo "far"; } ?> fa-thumbs-up"></i></a></li>
+    <li class="elements_barre_posts"><a href="posts.php?id=<?php echo $feed["id"]; ?>"><?php echo $feed["nb_com"];?> Commentaires <i class="far fa-comments"></i></a></li>
+    <li class="elements_barre_posts"><a href="" onclick="share_post(<?php echo $feed["id"].",".$_SESSION["idcon"]; ?>)"><?php echo $feed["nb_share"];?> Shares <i class="fas fa-share"></i></a></li>
+  </ul>
+</div><?php
+} ?><hr>
