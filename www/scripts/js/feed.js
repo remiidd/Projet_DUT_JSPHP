a526ba12c804738d0e8299b6339ee6d5d18d912a
Rémi Debray <remi281099@orange.fr>
2018-12-11 17:30:37 +0100

    11-12-18 17:30:37

--- www/scripts/js/feed.js
+++ www/scripts/js/feed.js
@@ -5,7 +5,7 @@ function verif_bar() {
   setTimeout( function(){
       console.log("salut");
       console.log($(document).height() - $(window).height() - $(window).scrollTop());
-      if (($(document).height() - $(window).height() - $(window).scrollTop();) == 0) {
+      if (($(document).height() - $(window).height() - $(window).scrollTop()) == 0) {
          console.log("en bas");
       }
   }, 3000);
