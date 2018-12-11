ff49ecba627bf7a24834a605925b07dcd087138f
Rémi Debray <remi281099@orange.fr>
2018-12-11 14:23:05 +0100

    11-12-18 14:23:05

--- www/scripts/js/comments.js
+++ www/scripts/js/comments.js
@@ -17,14 +17,15 @@
 		    var x = t[ i ].split('=');
 		      f[x[0]]=x[1];
     }
+    var res = f[0];
 	  return f;
   }
 
   function charger_com(){
       var id_post = recup_id_post();
-      var url_com = "scripts/php/load_com.php?id="+id_post;
+      var url_com = "scripts/php/load_com.php?id="+id_post["id"];
       console.log(url_com);
-      console.log(id_post[0]);
+      console.log(id_post["id"]);
       setTimeout( function(){
           $.ajax({
               url : url_com,
