781a87b591ed641117ba53357220189ce954efe2
Rémi Debray <remi281099@orange.fr>
2018-12-11 17:19:50 +0100

    11-12-18 17:19:50

--- www/scripts/js/feed.js
+++ www/scripts/js/feed.js
@@ -5,7 +5,7 @@ function verif_bar() {
   setTimeout( function(){
       console.log("salut");
       b = $('body');
-      console.log((b.scrollTop() + b.clientHeight() - b.scrollHeight()));
+      console.log((b.scrollTop() + this.clientHeight() - b.scrollHeight()));
       if ((this.scrollTop + this.clientHeight - this.scrollHeight) == 0) {
          console.log("en bas");
       }
