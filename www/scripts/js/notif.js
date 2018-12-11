f52a5c9f622c2a940c8eeb2d8eb0c10989810f3f
aloisguitton <aloisguitton@orange.fr>
2018-12-11 15:17:57 +0100

    11/12/2018 15:17:56

--- www/scripts/js/notif.js
+++ www/scripts/js/notif.js
@@ -22,7 +22,7 @@ function notif_invitation(){
             url : "scripts/php/notif_invitation.php",
             type : "GET",
             success : function(html){
-                $('#notification_count_menu_invitation').html(html);
+                $('#notification_count_menu_invit').html(html);
             }
         });
 
