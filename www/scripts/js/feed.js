bc81442dbf68f5934cd344177e3a67d98882f525
Rémi Debray <remi281099@orange.fr>
2018-12-12 13:36:07 +0100

    12-12-18 13:36:07

--- www/scripts/js/feed.js
+++ www/scripts/js/feed.js
@@ -1,6 +1,14 @@
 function verif_bar() {
   var bottom = $(document).height() - $(window).height() - $(window).scrollTop();
     if(bottom == 0) {
+      var urll = ;
+      $.ajax({
+          url : urll,
+          type : "GET",
+          success : function(html){
+              $('.wrapp').append(html);
+          }
+      });
       $('.wrapp').append('<?php if($_GET["feed"]!=100){ include "scripts/php/feed_defilement.php"; } ?>');
     }
 }
