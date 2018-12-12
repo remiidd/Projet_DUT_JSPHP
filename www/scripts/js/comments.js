7c36d397a8162d9691e5f7b6add1a1e27885fbc5
Rémi Debray <remi281099@orange.fr>
2018-12-12 23:19:07 +0100

    12-12-18 23:19:07

--- www/scripts/js/comments.js
+++ www/scripts/js/comments.js
@@ -1,14 +1,3 @@
-  /* function getParameterByName(name) {
-    name = name.replace(/[[]/, [).replace(/[]]/, ]);
-    var regexS = [?&] + name + =([^&#]*);
-    var regex = new RegExp(regexS);
-    var results = regex.exec(window.location.search);
-    if(results == null) {
-      return;
-    } else {
-      return decodeURIComponent(results[1].replace(/+/g, ));
-    }
-  } */
 
   function recup_id_post() {
     var t = location.search.substring(1).split('&');
