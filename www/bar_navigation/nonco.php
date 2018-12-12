37a7908cafad25c1926e8fc64d4f87eb9a4388fb
aloisguitton <aloisguitton@orange.fr>
2018-12-12 12:51:49 +0100

    12/12/2018 12:51:49

--- www/bar_navigation/nonco.php
+++ www/bar_navigation/nonco.php
@@ -23,7 +23,7 @@
             <li class="nav-item active">
               <form class="nav-link"  method="post" action="scripts/php/pre-recherche.php">
                 <div class="search-box">
-                  <input class="rechin" type="text" name="recherche" placeholder="Rechercher"/>
+                  <input id="rechin" class="rechin" type="text" name="recherche" placeholder="Rechercher"/>
                   <i class="fas fa-search" ></i>
                   <input id="rechbut" type="submit" value=""/>
                 </div>
