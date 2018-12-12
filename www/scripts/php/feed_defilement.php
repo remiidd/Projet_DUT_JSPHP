122eeeff9a75c848a3c62d8bf9c5ca67b6d3ef45
Rémi Debray <remi281099@orange.fr>
2018-12-12 22:56:05 +0100

    12-12-18 22:56:05

--- www/scripts/php/feed_defilement.php
+++ www/scripts/php/feed_defilement.php
@@ -3,7 +3,7 @@ session_start();
 if(!isset($_SESSION["feedd"])){
   $_SESSION["feedd"] = 0;
   $_SESSION["stop_pub"] = false;
-  //header
+  header("Location : accueil");
 } else {
   $off = $_SESSION["feedd"];
   $_SESSION["feedd"] = $_SESSION["feedd"] + 5;
