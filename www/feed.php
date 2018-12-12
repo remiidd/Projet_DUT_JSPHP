fee68623ddef9de59e045034ea4987950f3effc6
Rémi Debray <remi281099@orange.fr>
2018-12-12 23:00:05 +0100

    12-12-18 23:00:05

--- www/feed.php
+++ www/feed.php
@@ -32,6 +32,10 @@ if(!isset($_SESSION["idcon"])){
     <div class="content">
       <div class="wrapp">
         <h3>Bananafeed</h3>
+        <?php if(!isset($_SESSION["feedd"])){
+          $_SESSION["feedd"] = 0;
+          $_SESSION["stop_pub"] = false;
+        } ?>
         <?php if($_GET["feed"]!=100){ include "scripts/php/feed_defilement.php"; } else { include "scripts/php/suggestion_profil.php";}?>
       </div>
     </div>
