d4444e9d9b8879d416f7555cae533b24221908fb
Rémi Debray <remi281099@orange.fr>
2018-12-13 16:46:31 +0100

    13-12-18 16:46:31

--- www/scripts/php/suggestion_profil.php
+++ www/scripts/php/suggestion_profil.php
@@ -18,13 +18,29 @@
       $profil = $req->fetch();
       $req2 = $bdd->query('SELECT statut FROM amis WHERE id_amis=\''.$_SESSION["idcon"].'\' AND id=\''.$profil["id"].'\'');
       $isfriend = $req2->fetch();
-      $estami = false;
+      $stamis = false;
       if(($isfriend["statut"]!=null)||$isfriend["statut"]!="bloque"){
-        $estami = true;
+        $stamis = true;
       }
       ?>
       <div class="profil_suggestion"><a href="profil-<?php echo $profil["id"]; ?>" class="no_deco_link modif_infos_boutons"><img src="../../<?php if($profil["photo_profil"]!="") { echo $profil["photo_profil"]; } else { echo "src/media/default_profil_picture.jpg"; } ?>" class="photo_profil_suggestion" alt="">
         <p><?php echo $profil["prenom"]." ".$profil["nom"]; ?></p></a></div>
+        <?php
+          if($stamis == true){
+            ?>
+            <form class="" action="scripts/php/supprimer_amis.php?id=<?php echo $_SESSION['idcon'];?>&id_amis=<?php  echo $donnees['id'];?>" method="post">
+              <input class="button_result_rech" type="submit" name="Ajouter" value="Supprimer">
+            </form>
+            <?php
+          }
+          else {
+            ?>
+            <form class="" action="scripts/php/ajouter_amis.php?id=<?php echo $_SESSION['idcon'];?>&id_amis=<?php  echo $donnees['id'];?>" method="post">
+              <input class="button_result_rech" type="submit" name="Ajouter" value="Ajouter">
+            </form>
+            <?php
+          }
+        ?>
     <?php } ?>
     <div class="profil_suggestion"><a href="profil-69" class="no_deco_link modif_infos_boutons"><img src="../../src/media/profils/69-pp.gif" class="photo_profil_suggestion" alt="">
       <p>La Banane Officielle</p></a></div>
