a31c4d56dc0783db596c94a4d0da6e4622929e26
aloisguitton <aloisguitton@orange.fr>
2018-12-19 14:57:39 +0100

    19/12/2018 14:57:39

--- www/scripts/js/profil.js
+++ www/scripts/js/profil.js
@@ -1,4 +1,9 @@
 function resize_img(){
-
-  console.log("taille");
+  var img = document.getElementsByClassName("pp");
+  var taille = document.body.clientWidth / 10;
+  for (i = 0; i < img.length; i++) {
+    img[i].style.width = taille + "px";
+    img[i].style.height = taille + "px";
+  }
+  console.log(taille);
 }
