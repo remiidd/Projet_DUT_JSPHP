5499c083528e6abc5304371a2b2582024b06ade7
Rémi Debray <remi281099@orange.fr>
2018-12-19 15:01:57 +0100

    19-12-18 15:01:57

--- www/scripts/php/like.php
+++ www/scripts/php/like.php
@@ -15,6 +15,9 @@ if(isset($_SESSION["idcon"])) {
 
 if(($id_post!=null)&&($id_profil!=null)&&($id_post!=0)&&($id_profil!=0)) {
   $bdd = new PDO('mysql:host=lulipa.server.r-heberg.fr;dbname=derayalois;charset=utf8', 'derayalois', 'testdebrayalois');
+  $req = $bdd->query('SELECT COUNT(*) AS nb_like FROM posts WHERE id=\''.$id_post.'\'');
+  $nb = $req->fetch();
+  $nblike = $nb["nb_like"];
 
   $ok = false;
   $reponse = $bdd->query('SELECT * FROM like_table WHERE profil_like=\''.$id_profil.'\' AND id_post=\''.$id_post.'\'');
@@ -24,15 +27,14 @@ if(($id_post!=null)&&($id_profil!=null)&&($id_post!=0)&&($id_profil!=0)) {
     $req = $bdd->query('INSERT INTO like_table(id, id_post, profil_like) VALUES(NULL, \''.$id_post.'\', \''.$id_profil.'\')');
     //UPDATE `derayalois`.`posts` SET `nb_like` = '1' WHERE `posts`.`id` = 3;
     $req = $bdd->query('UPDATE posts SET nb_like=nb_like+1 WHERE id=\''.$id_post.'\'');
+    echo $nblike+1;
   }
   if($ok){
     //DELETE FROM `derayalois`.`like_table` WHERE `like_table`.`id` = 8;
     $req = $bdd->query('DELETE FROM like_table WHERE profil_like=\''.$id_profil.'\' AND id_post=\''.$id_post.'\'');
     $req = $bdd->query('UPDATE posts SET nb_like=nb_like-1 WHERE id=\''.$id_post.'\'');
+    echo $nblike-1;
   }
-  $req = $bdd->query('SELECT COUNT(*) AS nb_like FROM posts WHERE id=\''.$id_post.'\'');
-  $nb = $req->fetch();
-  echo $nb["nb_like"];
 }
 
 
