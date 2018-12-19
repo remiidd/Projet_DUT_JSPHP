67e8f3b30e7c340a35eea0010bc951e8bc7de2fd
aloisguitton <aloisguitton@orange.fr>
2018-12-19 15:01:17 +0100

    19/12/2018 15:01:16

--- www/scripts/js/profil.js
+++ www/scripts/js/profil.js
@@ -1,6 +1,6 @@
 function resize_img(){
   var img = document.getElementsByClassName("pp");
-  var taille = document.body.clientWidth / 10;
+  var taille = document.body.clientWidth / 8;
   for (i = 0; i < img.length; i++) {
     img[i].style.width = taille + "px";
     img[i].style.height = taille + "px";
