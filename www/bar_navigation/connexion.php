6a26378ecc969e0d37f35a1141a424f178982cee
aloisguitton <aloisguitton@orange.fr>
2018-12-19 14:18:38 +0100

    19/12/2018 14:18:38

--- www/bar_navigation/connexion.php
+++ www/bar_navigation/connexion.php
@@ -1,5 +1,5 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
   <div class="navbar-brand">
-    <a href="index.php" >BananaBook 🍌</a>
+    <a id="bananatitre" href="index.php" >BananaBook 🍌</a>
   </div>
 </nav>
