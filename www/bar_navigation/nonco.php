8d0090ecd78cba94e06cc95a1bdfa7123e149826
aloisguitton <aloisguitton@orange.fr>
2018-12-19 14:19:44 +0100

    19/12/2018 14:19:44

--- www/bar_navigation/nonco.php
+++ www/bar_navigation/nonco.php
@@ -94,7 +94,7 @@
     ?>
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
       <div class="navbar-brand">
-        <a id="bananatitre" href="index.php" >BananaBook 🍌</a>
+        <a href="index.php" >BananaBook 🍌</a>
       </div>
 
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
