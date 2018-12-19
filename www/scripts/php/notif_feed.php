be58300a8eacdd4be3239becedc66c705566d42b
Rémi Debray <remi281099@orange.fr>
2018-12-19 14:18:19 +0100

    19-12-18 14:18:19

---
+++ www/scripts/php/notif_feed.php
@@ -0,0 +1,21 @@
+<?php
+  session_start();
+  $mess = "<i class=\"fas fa-user-friends\"></i>";
+  $moi = $_SESSION['idcon'];
+  try{
+    $bdd = new PDO('mysql:host=lulipa.server.r-heberg.fr;dbname=derayalois;port=3306;charset=utf8', 'derayalois', 'testdebrayalois');
+  }
+  catch (Exception $e){
+        die('Erreur : ' . $e->getMessage());
+  }
+  $moi = $_SESSION['idcon'];
+  $reponse = $bdd->query("SELECT COUNT(*) as nb_demande FROM amis WHERE `id`=$moi AND statut=\"demande\"");
+  $donnees = $reponse->fetch();
+  if($donnees['nb_demande']!= 0){
+    $mess.="<span id=\"notification_count_menu_invit\">".$donnees['nb_demande']."</span>";
+  }
+  echo $mess;
+
+
+  //<i class="fas fa-bell"></i>
+?>
