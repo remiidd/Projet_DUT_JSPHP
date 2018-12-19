9cf3a8e17642173e22d1417f314c4c967738ba34
aloisguitton <aloisguitton@orange.fr>
2018-12-19 14:45:55 +0100

    19/12/2018 14:45:54

---
+++ www/scripts/js/profil.js
@@ -0,0 +1,8 @@
+function resize_img(){
+  var img = document.getElementsByClassName("pp");
+  var taille = document.body.clientWidth / 10;
+  for (i = 0; i < img.length; i++) {
+    img[i].style.width = taille + "px";
+    img[i].style.height = taille + "px";
+  }
+}
